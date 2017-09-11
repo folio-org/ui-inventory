bcac087ce48764116bdef36baa9d13e48df0bb42
Charles Ledvina <charles@indexdata.com>
2017-09-11 14:38:51 -0500

    Added "New title" test that creates a new title, sorts list, and confirms creation.

---
+++ test/ui-testing/new_title.js
@@ -0,0 +1,50 @@
+module.exports.test = function(uiTestCtx) {
+
+  describe('Module test: instances:stub', function() {
+    const { config, helpers: { login, openApp, logout }, meta: { testVersion } } = uiTestCtx;
+    const nightmare = new Nightmare(config.nightmare);
+
+    this.timeout(Number(config.test_timeout));
+
+    const title = '0000000 : a new test title'
+
+    describe('Login > Open module "Instances" > Create new instance > Sort list by title > Confirm creation of new title > Logout', () => {
+      before( done => {
+        login(nightmare, config, done);  // logs in with the default admin credentials
+      })
+      after( done => {
+        logout(nightmare, config, done);
+      })
+      it('should open module "Instances" and find version tag ', done => {
+        nightmare
+        .use(openApp(nightmare, config, done, 'instances', testVersion))
+        .then(result => result )
+      })
+      it('should create new instance ', done => {
+        nightmare
+	.wait('#clickable-new-instance')
+	.click('#clickable-new-instance')
+	.wait(222)
+	.insert('input[name=title]', title)
+	.wait(222)
+	.click('#clickable-create-instance')
+        .then(result => { done() } )
+      }) 
+      it('should find new title in list ', done => {
+        nightmare
+	.wait('#list-instances')
+	.xclick('//div[@role="presentation"][.="title"]')
+	.wait(1111)
+	.evaluate(function(title) {
+	    var ti = document.querySelector('#list-instances > .scrollable---9-pBJ > div > div > div > div[title="' + title + '"]')
+	    if (ti == null) {
+	    	throw new Error("Can't find newly created title (" + title + ") at top of sorted list")
+	    }
+	}, title)
+        .then(result => { done() } )
+	.catch(done)
+      })
+    })
+  })
+}
+
