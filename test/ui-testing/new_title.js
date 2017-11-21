0962a43c4d4fc6c46c7eadf8e08b56c42c348c4a
Charles Ledvina <charles@indexdata.com>
2017-11-21 16:28:01 -0600

    Fix duplicated ids and have new_title test create full record.

--- test/ui-testing/new_title.js
+++ test/ui-testing/new_title.js
@@ -1,13 +1,24 @@
 module.exports.test = function(uiTestCtx) {
 
-  describe('Module test: instances:inventory', function() {
+  describe('Module test: inventory:new_title', function() {
     const { config, helpers: { login, openApp, logout }, meta: { testVersion } } = uiTestCtx;
     const nightmare = new Nightmare(config.nightmare);
 
     this.timeout(Number(config.test_timeout));
 
-    const title = '0000000 : a new test title'
-    const author = 'Test author name'
+    const title = '00'
+    const ed = '[1st ed.]'
+    const series = 'Catalogus (Museum van Hedendaagse Kunst Antwerpen), 92'
+    const id = new Date().valueOf()
+    const isbn = '9789072828279'
+    const author = 'Venlet, Richard'
+    const contrib = 'Moritz Küng'
+    const subjects = ['Venlet, Richard -- Exhibitions', 'Installations (Art) -- Exhibitions', 'Installations (Art)']
+    const pub = { 'publisher':'MUHKA', 'place':'Antwerpen', 'date':'©2002' }
+    const url = 'http://www.worldcat.org/title/00/oclc/921279647'
+    const desc = '230 pages : chiefly illustrations (some color) ; 28 cm + 1 folded sheet'
+    const lang = ['dut', 'eng']
+    const notes = ['Catalog of two exhibitions, "Paramount basics" held at the 25th Biennale of São Paulo, 23 March-2 June 2002, and "Paramount basics (extended)," held at MUHKA, Museum van Hedendaagse Kunst Antwerpen, 21 September-24 November 2002.', 'Cover title.', 'Includes folded sheet of list of works in the exhibition held at MUHKA.' ]
 
     describe('Login > Open module "Inventory" > Create new instance > Sort list by title > Confirm creation of new title > Logout', () => {
       before( done => {
@@ -25,18 +36,66 @@ module.exports.test = function(uiTestCtx) {
         nightmare
         .wait('#clickable-new-instance')
         .click('#clickable-new-instance')
-        .wait(222)
+	.wait(55)
         .insert('input[name=title]', title)
-        .wait(222)
+	.insert('#input_instance_edition', ed)
+        .click('#clickable-add-series')
+	.insert('input[name="series[0]"]', series)
+	.wait(55)
+	.click('#clickable-add-identifier')
+	.wait(55)
+	.insert('input[name="identifiers[0].value', id)
+	.type('select[name="identifiers[0].identifierTypeId', 's')
+	.click('#clickable-add-identifier')
+	.wait(55)
+	.insert('input[name="identifiers[1].value', isbn)
+	.type('select[name="identifiers[1].identifierTypeId', 'ii')
         .click('#clickable-add-creator')
-        .wait(222)
+	.wait(55)
         .insert('#input_creator_name_0', author)
         .type('#select_creator_type_0', 'P')
-        .wait(222)
         .type('#select_instance_type', 'B')
-        .wait(222)
+	.click('#clickable-add-contributor')
+	.insert('input[name="contributors[0].name"', contrib)
+	.type('select[name="contributors[0].contributorTypeId"]', 'r')
+        .click('#clickable-add-subject')
+	.insert('input[name="subjects[0]"]', subjects[0])
+        .click('#clickable-add-subject')
+	.insert('input[name="subjects[1]"]', subjects[1])
+        .click('#clickable-add-subject')
+	.insert('input[name="subjects[2]"]', subjects[2])
+        //.click('#clickable-add-classification')
+        .click('#clickable-add-publication')
+	.wait(55)
+	.insert('input[name="publication[0].publisher"]', pub.publisher)
+	.insert('input[name="publication[0].place"]', pub.place)
+	.insert('input[name="publication[0].dateOfPublication"]', pub.date)
+        .click('#clickable-add-url')
+	.wait(55)
+	.insert('input[name="urls[0]"]', url)
+        .click('#clickable-add-description')
+	.wait(55)
+	.insert('input[name="physicalDescriptions[0]"]', desc)
+	.click('#clickable-add-language')
+	.wait(55)
+	.select('select[name="languages[0]"]', lang[0])
+	.click('#clickable-add-language')
+	.wait(55)
+	.select('select[name="languages[1]"]', lang[1])
+        .click('#clickable-add-notes')
+	.wait(55)
+	.insert('input[name="notes[0]"]', notes[0])
+        .click('#clickable-add-notes')
+	.wait(55)
+	.insert('input[name="notes[1]"]', notes[1])
+        .click('#clickable-add-notes')
+	.wait(55)
+	.insert('input[name="notes[2]"]', notes[2])
+	.wait(55)
         .click('#clickable-create-instance')
+	.wait(2222)
         .then(result => { done() } )
+	.catch(done)
       }) 
       it('should find new title in list ', done => {
         nightmare
