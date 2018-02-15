7cbf8bfd2a59992172092a638521de8d1d5cd217
Charles Ledvina <charles@indexdata.com>
2018-02-15 15:30:54 -0600

    Fix lint errors

--- test/ui-testing/new_title.js
+++ test/ui-testing/new_title.js
@@ -1,119 +1,116 @@
-module.exports.test = function(uiTestCtx) {
-
-  describe('Module test: inventory:new_title', function() {
+/* global Nightmare describe it before after */
+module.exports.test = function uiTest(uiTestCtx) {
+  describe('Module test: inventory:new_title', function testDescribe() {
     const { config, helpers: { login, openApp, logout }, meta: { testVersion } } = uiTestCtx;
     const nightmare = new Nightmare(config.nightmare);
 
     this.timeout(Number(config.test_timeout));
 
-    const title = '0'
-    const ed = '[1st ed.]'
-    const series = 'Catalogus (Museum van Hedendaagse Kunst Antwerpen), 92'
-    const id = new Date().valueOf()
-    const isbn = '9789072828279'
-    const author = 'Venlet, Richard'
-    const contrib = 'Moritz Küng'
-    const subjects = ['Venlet, Richard -- Exhibitions', 'Installations (Art) -- Exhibitions', 'Installations (Art)']
-    const pub = { 'publisher':'MUHKA', 'place':'Antwerpen', 'date':'©2002' }
-    const url = 'http://www.worldcat.org/title/00/oclc/921279647'
-    const desc = '230 pages : chiefly illustrations (some color) ; 28 cm + 1 folded sheet'
-    const lang = ['dut', 'eng']
-    const notes = ['Catalog of two exhibitions, "Paramount basics" held at the 25th Biennale of São Paulo, 23 March-2 June 2002, and "Paramount basics (extended)," held at MUHKA, Museum van Hedendaagse Kunst Antwerpen, 21 September-24 November 2002.', 'Cover title.', 'Includes folded sheet of list of works in the exhibition held at MUHKA.' ]
+    const title = '0';
+    const ed = '[1st ed.]';
+    const series = 'Catalogus (Museum van Hedendaagse Kunst Antwerpen), 92';
+    const id = new Date().valueOf();
+    const isbn = '9789072828279';
+    const contrib = 'Moritz Küng';
+    const subjects = ['Venlet, Richard -- Exhibitions', 'Installations (Art) -- Exhibitions', 'Installations (Art)'];
+    const pub = { publisher: 'MUHKA', place: 'Antwerpen', date: '©2002' };
+    const url = 'http://www.worldcat.org/title/00/oclc/921279647';
+    const desc = '230 pages : chiefly illustrations (some color) ; 28 cm + 1 folded sheet';
+    const lang = ['dut', 'eng'];
+    const notes = ['Catalog of two exhibitions, "Paramount basics" held at the 25th Biennale of São Paulo, 23 March-2 June 2002, and "Paramount basics (extended)," held at MUHKA, Museum van Hedendaagse Kunst Antwerpen, 21 September-24 November 2002.', 'Cover title.', 'Includes folded sheet of list of works in the exhibition held at MUHKA.'];
 
     describe('Login > Open module "Inventory" > Create new instance > Sort list by title > Confirm creation of new title > Logout', () => {
-      before( done => {
-        login(nightmare, config, done);  // logs in with the default admin credentials
-      })
-      after( done => {
+      before((done) => {
+        login(nightmare, config, done); // logs in with the default admin credentials
+      });
+      after((done) => {
         logout(nightmare, config, done);
-      })
-      it('should open module "Inventory" and find version tag ', done => {
+      });
+      it('should open module "Inventory" and find version tag ', (done) => {
         nightmare
-        .use(openApp(nightmare, config, done, 'inventory', testVersion))
-        .then(result => result )
-      })
-      it('should create new instance ', done => {
+          .use(openApp(nightmare, config, done, 'inventory', testVersion))
+          .then(result => result);
+      });
+      it('should create new instance ', (done) => {
         nightmare
-        .wait('#clickable-newinventory')
-        .click('#clickable-newinventory')
-	.wait(55)
-        .insert('input[name=title]', title)
-	.insert('#input_instance_edition', ed)
-        .click('#clickable-add-series')
-	.insert('input[name="series[0]"]', series)
-	.wait(55)
-	.click('#clickable-add-identifier')
-	.wait(55)
-	.insert('input[name="identifiers[0].value', id)
-	.type('select[name="identifiers[0].identifierTypeId', 's')
-	.click('#clickable-add-identifier')
-	.wait(55)
-	.insert('input[name="identifiers[1].value', isbn)
-	.type('select[name="identifiers[1].identifierTypeId', 'ii')
-        .type('#select_instance_type', 'B')
-	.click('#clickable-add-contributor')
-	.insert('input[name="contributors[0].name"', contrib)
-	.type('select[name="contributors[0].contributorNameTypeId"]', 'P')
-        .click('#clickable-add-subject')
-	.insert('input[name="subjects[0]"]', subjects[0])
-        .click('#clickable-add-subject')
-	.insert('input[name="subjects[1]"]', subjects[1])
-        .click('#clickable-add-subject')
-	.insert('input[name="subjects[2]"]', subjects[2])
-        .click('#clickable-add-publication')
-	.wait(55)
-	.insert('input[name="publication[0].publisher"]', pub.publisher)
-	.insert('input[name="publication[0].place"]', pub.place)
-	.insert('input[name="publication[0].dateOfPublication"]', pub.date)
-        .click('#clickable-add-url')
-	.wait(55)
-	.insert('input[name="urls[0]"]', url)
-        .click('#clickable-add-description')
-	.wait(55)
-	.insert('input[name="physicalDescriptions[0]"]', desc)
-	.click('#clickable-add-language')
-	.wait(55)
-	.select('select[name="languages[0]"]', lang[0])
-	.click('#clickable-add-language')
-	.wait(55)
-	.select('select[name="languages[1]"]', lang[1])
-        .click('#clickable-add-notes')
-	.wait(55)
-	.insert('input[name="notes[0]"]', notes[0])
-        .click('#clickable-add-notes')
-	.wait(55)
-	.insert('input[name="notes[1]"]', notes[1])
-        .click('#clickable-add-notes')
-	.wait(55)
-	.insert('input[name="notes[2]"]', notes[2])
-	.wait(55)
-        .click('#clickable-create-instance')
-	.wait(2222)
-	.click('button[title^="Close"]')
-	.wait(1111)
-        .then(result => { done() } )
-	.catch(done)
-      }) 
-      it('should find new title in list ', done => {
+          .wait('#clickable-newinventory')
+          .click('#clickable-newinventory')
+          .wait(55)
+          .insert('input[name=title]', title)
+          .insert('#input_instance_edition', ed)
+          .click('#clickable-add-series')
+          .insert('input[name="series[0]"]', series)
+          .wait(55)
+          .click('#clickable-add-identifier')
+          .wait(55)
+          .insert('input[name="identifiers[0].value', id)
+          .type('select[name="identifiers[0].identifierTypeId', 's')
+          .click('#clickable-add-identifier')
+          .wait(55)
+          .insert('input[name="identifiers[1].value', isbn)
+          .type('select[name="identifiers[1].identifierTypeId', 'ii')
+          .type('#select_instance_type', 'B')
+          .click('#clickable-add-contributor')
+          .insert('input[name="contributors[0].name"', contrib)
+          .type('select[name="contributors[0].contributorNameTypeId"]', 'P')
+          .click('#clickable-add-subject')
+          .insert('input[name="subjects[0]"]', subjects[0])
+          .click('#clickable-add-subject')
+          .insert('input[name="subjects[1]"]', subjects[1])
+          .click('#clickable-add-subject')
+          .insert('input[name="subjects[2]"]', subjects[2])
+          .click('#clickable-add-publication')
+          .wait(55)
+          .insert('input[name="publication[0].publisher"]', pub.publisher)
+          .insert('input[name="publication[0].place"]', pub.place)
+          .insert('input[name="publication[0].dateOfPublication"]', pub.date)
+          .click('#clickable-add-url')
+          .wait(55)
+          .insert('input[name="urls[0]"]', url)
+          .click('#clickable-add-description')
+          .wait(55)
+          .insert('input[name="physicalDescriptions[0]"]', desc)
+          .click('#clickable-add-language')
+          .wait(55)
+          .select('select[name="languages[0]"]', lang[0])
+          .click('#clickable-add-language')
+          .wait(55)
+          .select('select[name="languages[1]"]', lang[1])
+          .click('#clickable-add-notes')
+          .wait(55)
+          .insert('input[name="notes[0]"]', notes[0])
+          .click('#clickable-add-notes')
+          .wait(55)
+          .insert('input[name="notes[1]"]', notes[1])
+          .click('#clickable-add-notes')
+          .wait(55)
+          .insert('input[name="notes[2]"]', notes[2])
+          .wait(55)
+          .click('#clickable-create-instance')
+          .wait(2222)
+          .click('button[title^="Close"]')
+          .wait(1111)
+          .then(() => { done(); })
+          .catch(done);
+      });
+      it('should find new title in list ', (done) => {
         nightmare
-        .wait('#list-inventory')
-        .xclick('//div[@role="presentation"][.="title"]')
-        .wait(3000)
-        .xclick('//div[@role="presentation"][.="title"]')
-        .wait(3000)
-        .evaluate(function(title) {
-            var sel = '#list-inventory div[role="listitem"]:nth-child(1) > a > div[title="' + title + '"]'
-            var ti = document.querySelector(sel)
-            if (ti == null) {
-              throw new Error("Can't find newly created title (" + title + ") at top of sorted list with selector: " + sel)
+          .wait('#list-inventory')
+        // .xclick('//div[@role="presentation"][.="title"]')
+        // .wait(3000)
+          .xclick('//div[@role="presentation"][.="title"]')
+          .wait(3000)
+          .evaluate(function evalSort(titl) {
+            const sel = `#list-inventory div[role="listitem"]:nth-child(1) > a > div[title="${titl}"]`;
+            const ti = document.querySelector(sel);
+            if (ti === null) {
+              throw new Error(`Can't find newly created title (${titl}) at top of sorted list with selector: ${sel}`);
             }
-        }, title)
-        .then(result => { 
-	  done()
-	})
-	.catch(done)
-      })
-    })
-  })
-}
+          }, title)
+          .then(() => { done(); })
+          .catch(done);
+      });
+    });
+  });
+};
 
--- test/ui-testing/stub.js
+++ test/ui-testing/stub.js
@@ -1,24 +1,24 @@
-module.exports.test = function(uiTestCtx) {
-
-  describe('Module test: instances:stub', function() {
+/* global Nightmare describe it before after */
+module.exports.test = function test(uiTestCtx) {
+  describe('Module test: instances:stub', function startTest() {
     const { config, helpers: { login, openApp, logout }, meta: { testVersion } } = uiTestCtx;
     const nightmare = new Nightmare(config.nightmare);
 
     this.timeout(Number(config.test_timeout));
 
     describe('Login > Open module "Instances" > Logout', () => {
-      before( done => {
-        login(nightmare, config, done);  // logs in with the default admin credentials
-      })
-      after( done => {
+      before((done) => {
+        login(nightmare, config, done); // logs in with the default admin credentials
+      });
+      after((done) => {
         logout(nightmare, config, done);
-      })
-      it('should open module "Instances" and find version tag ', done => {
+      });
+      it('should open module "Instances" and find version tag ', (done) => {
         nightmare
-        .use(openApp(nightmare, config, done, 'instances', testVersion))
-        .then(result => result )
-      })
-    })
-  })
-}
+          .use(openApp(nightmare, config, done, 'instances', testVersion))
+          .then(result => result);
+      });
+    });
+  });
+};
 
