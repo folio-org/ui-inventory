209c25a7cc81f339eb7dde867102a83041ca5132
Niels Erik G. Nielsen <nielserik@indexdata.com>
2018-03-24 00:56:11 +0100

    Add contributor type to instance form. UIIN-92
      Requires instance-storage v4.2 for new contributorTypeText field

--- edit/contributorFields.js
+++ edit/contributorFields.js
@@ -4,8 +4,15 @@ import TextField from '@folio/stripes-components/lib/TextField';
 import Select from '@folio/stripes-components/lib/Select';
 import RepeatableField from '@folio/stripes-components/lib/structures/RepeatableField';
 
-const ContributorFields = ({ contributorNameTypes }) => {
+const ContributorFields = ({ contributorNameTypes, contributorTypes }) => {
   const contributorNameTypeOptions = contributorNameTypes.map(
+    it => ({
+      label: it.name,
+      value: it.id,
+    }),
+  );
+
+  const contributorTypeOptions = contributorTypes.map(
     it => ({
       label: it.name,
       value: it.id,
@@ -25,19 +32,31 @@ const ContributorFields = ({ contributorNameTypes }) => {
           component: TextField,
         },
         {
-          label: 'Type',
+          label: 'Name Type',
           name: 'contributorNameTypeId',
           component: Select,
           dataOptions: [{ label: 'Select name type', value: '' }, ...contributorNameTypeOptions],
         },
+        {
+          label: 'Contributor Type',
+          name: 'contributorTypeId',
+          component: Select,
+          dataOptions: [{ label: 'Select contributor type', value: '' }, ...contributorTypeOptions],
+        },
+        {
+          label: 'Type, free text',
+          name: 'contributorTypeText',
+          component: TextField,
+        },
       ]}
-      newItemTemplate={{ name: '', contributorNameTypeId: '' }}
+      newItemTemplate={{ name: '', contributorNameTypeId: '', contributorTypeId: '', contributorTypeText: '' }}
     />
   );
 };
 
 ContributorFields.propTypes = {
   contributorNameTypes: PropTypes.arrayOf(PropTypes.object),
+  contributorTypes: PropTypes.arrayOf(PropTypes.object),
 };
 
 export default ContributorFields;
