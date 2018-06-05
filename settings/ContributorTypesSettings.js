19a4caa4056878c23057a7ffc8ad7b3d1a317c6d
Niels Erik G. Nielsen <nielserik@indexdata.com>
2018-06-05 12:54:52 +0200

    Adapt settings page to contributor-types v2.0. UIIN-149
      Make MARC relator terms read-only in settings.

--- settings/ContributorTypesSettings.js
+++ settings/ContributorTypesSettings.js
@@ -17,6 +17,9 @@ class FormatTypesSettings extends React.Component {
     this.connectedControlledVocab = props.stripes.connect(ControlledVocab);
   }
 
+  suppressEdit = term => term.source === 'marcrelator';
+  suppressDelete = term => term.source === 'marcrelator';
+
   render() {
     const { formatMessage } = this.props.stripes.intl;
 
@@ -33,6 +36,8 @@ class FormatTypesSettings extends React.Component {
         itemTemplate={{ source: 'local' }}
         hiddenFields={['description', 'numberOfObjects']}
         nameKey="name"
+        columnWidths={{ 'name': 300, 'code': 50 }}
+        actionSuppressor={{ edit: this.suppressEdit, delete: this.suppressDelete }}
         id="contributor-types"
       />
     );
