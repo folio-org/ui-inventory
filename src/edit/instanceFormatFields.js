d62a3037c3c4bc5af7432b882151054ac817d5e5
John Coburn <jcoburn@ebsco.com>
2018-10-05 08:17:24 -0500

    fix 'Select' import in instanceFormatFields.js

--- src/edit/instanceFormatFields.js
+++ src/edit/instanceFormatFields.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Field } from 'redux-form';
-import Select from '@folio/stripes-components/lib/Select';
+import { Select } from '@folio/stripes/components';
 import RepeatableField from '../components/RepeatableField';
 
 const renderInstanceFormatField = ({ field, fieldIndex }, formatMsg, instanceFormats) => {
