105b0b0d497b82634c996182260b419c1e102059
Zak Burke <zburke@cornell.edu>
2018-11-09 07:26:21 -0500

    tests should not use `title` attributes
    Hang search tests off `data-total-count` and `aria-label` instead of
    using `title`. That still sorta sucks, but it sucks less.
    
    Refs STCOM-296

--- test/ui-testing/inventorySearch.js
+++ test/ui-testing/inventorySearch.js
@@ -9,6 +9,7 @@ module.exports.test = function test(uiTestCtx) {
     describe('Login > Click Inventory > Enter Search Term > Wait for Results > Confirm search term at top of results > Click Reset All > Wait for results pan to change state > Logout\n', () => {
       const title = 'California';
       const authorName = 'Huntley, Henry Veel';
+      let hitCount = 0;
       before((done) => {
         login(nightmare, config, done); // logs in with the default admin credentials
       });
@@ -20,6 +21,18 @@ module.exports.test = function test(uiTestCtx) {
           .use(openApp(nightmare, config, done, 'inventory', testVersion))
           .then(result => result);
       });
+      it('should find hit count with no filters applied', (done) => {
+        nightmare
+          .wait('#list-inventory')
+          .evaluate(() => {
+            return document.querySelector('#list-inventory').getAttribute('data-total-count');
+          })
+          .then((result) => {
+            done();
+            hitCount = result;
+          })
+          .catch(done);
+      });
       it(`should search for: "${title}"`, (done) => {
         nightmare
           .wait('#clickable-inventory-module')
@@ -30,7 +43,8 @@ module.exports.test = function test(uiTestCtx) {
           .wait('#clickable-reset-all')
           .wait('button[type=submit]')
           .click('button[type=submit]')
-          .wait(`#list-inventory div[role="listitem"] div[role="gridcell"][title*="${title}"]`)
+          .wait(`#list-inventory:not([data-total-count^="${hitCount}"])`)
+          .wait(`a[aria-label*="Title: ${title}"]`)
           /* .evaluate(function evall(title2) {
             const list = document.querySelector('#list-inventory div[role="listitem"]:first-of-type > a > div[role="gridcell"]:nth-of-type(1)').title;
             console.log(`list contains: ${list} and title is ${title2} `);
@@ -46,19 +60,21 @@ module.exports.test = function test(uiTestCtx) {
         nightmare
           .wait('#clickable-reset-all')
           .click('#clickable-reset-all')
+          .wait(`#list-inventory[data-total-count^="${hitCount}"]`)
           .then(done)
           .catch(done);
       });
       it(`should search for title: ${title}`, (done) => {
         nightmare
-          .wait(55)
+          .wait('#input-inventory-search-qindex')
           .select('#input-inventory-search-qindex', 'title')
-          .wait(55)
+          .wait('#input-inventory-search')
           .insert('#input-inventory-search', title)
           .wait('#clickable-reset-all')
           .wait('button[type=submit]')
           .click('button[type=submit]')
-          .wait(`#list-inventory div[role="listitem"] div[role="gridcell"][title*="${title}"]`)
+          .wait(`#list-inventory:not([data-total-count^="${hitCount}"])`)
+          .wait(`a[aria-label*="Title: ${title}"]`)
           /* .evaluate(function evall(title2) {
             const list = document.querySelector('#list-inventory div[role="listitem"]:first-of-type > a > div[role="gridcell"]:nth-of-type(1)').title;
             // console.log(`list contains: ${list} and title is ${title2} `);
@@ -71,10 +87,9 @@ module.exports.test = function test(uiTestCtx) {
       });
       it('should click "reset all" button', (done) => {
         nightmare
-          .wait(55)
           .wait('#clickable-reset-all')
           .click('#clickable-reset-all')
-          .wait(555)
+          .wait(`#list-inventory[data-total-count^="${hitCount}"]`)
           .then(done)
           .catch(done);
       });
@@ -85,7 +100,8 @@ module.exports.test = function test(uiTestCtx) {
           .wait('#clickable-reset-all')
           .wait('button[type=submit]')
           .click('button[type=submit]')
-          .wait(`#list-inventory div[role="listitem"] div[role="gridcell"][title*="${authorName}"]`)
+          .wait(`#list-inventory:not([data-total-count^="${hitCount}"])`)
+          .wait(`a[aria-label*="${authorName}"]`)
           .then(done)
           .catch(done);
       });
