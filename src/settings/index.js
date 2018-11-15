67c59849742cd10c7c76ca817579ab72afa6fdbc
ADITYA MATUKUMALLI <aditya.matukumalli@gmail.com>
2018-11-15 05:33:34 -0500

    Create statistical code types settings page, refs UIIN-302 (#358)

--- src/settings/index.js
+++ src/settings/index.js
@@ -8,6 +8,7 @@ import ResourceTypesSettings from './ResourceTypesSettings';
 import ContributorTypesSettings from './ContributorTypesSettings';
 import URLRelationshipSettings from './URLRelationshipSettings';
 import InstanceStatusTypesSettings from './InstanceStatusTypesSettings';
+import StatisticalCodeTypes from './StatisticalCodeTypes';
 import AlternativeTitleTypesSettings from './AlternativeTitleTypesSettings';
 
 class InventorySettings extends React.Component {
@@ -80,6 +81,12 @@ class InventorySettings extends React.Component {
       {
         label: formatMessage({ id: 'ui-inventory.instanceHoldingsItem' }),
         pages: [
+          {
+            route: 'statisticalCodeTypes',
+            label: formatMessage({ id: 'ui-inventory.statisticalCodeTypes' }),
+            component: StatisticalCodeTypes,
+            perm: 'ui-inventory.settings.statistical-code-types',
+          },
           {
             route: 'URLrelationship',
             label: formatMessage({ id: 'ui-inventory.URLrelationship' }),
