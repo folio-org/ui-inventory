262e257c3b5494d5a7ebad0c510e2f38a020fbcf
Rasmus Wølk <rasmuswoelk@gmail.com>
2019-04-23 16:20:42 +0200

    Updated the mocked endpoint for '/inventory/items'. Replaced with a mock provided by Yurii Danylenko

--- test/bigtest/network/config.js
+++ test/bigtest/network/config.js
@@ -145,16 +145,59 @@ export default function configure() {
     totalRecords: 0
   });
 
-  this.get('/inventory/items', ({ items }, request) => {
-    if (request.queryParams.query) {
-      const cqlParser = new CQLParser();
-      cqlParser.parse(request.queryParams.query);
-      return items.where({
-        barcode: cqlParser.tree.term
-      });
-    } else {
-      return items.all();
-    }
+  this.get('/inventory/items', {
+    items: [
+      {
+        id: '7abd8dfe-0234-4256-b2e6-539499999cac',
+        status: {
+          name: 'Checked out',
+        },
+        title: '14 cows for America',
+        hrid: 'it00000168',
+        contributorNames: [
+          {
+            name: 'Deedy, Carmen Agra.',
+          },
+          {
+            name: 'Naiyomah, Wilson Kimeli.',
+          },
+          {
+            name: 'Gonzalez, Thomas',
+          },
+        ],
+        formerIds: [],
+        discoverySuppress: null,
+        holdingsRecordId: '44a21d17-a666-4f34-b24d-644f8ed1537b',
+        barcode: '5860825104574',
+        copyNumbers: [],
+        notes: [],
+        circulationNotes: [],
+        numberOfPieces: '3',
+        yearCaption: [],
+        electronicAccess: [],
+        statisticalCodeIds: [],
+        purchaseOrderLineIdentifier: null,
+        materialType: {
+          id: '1a54b431-2e4f-452d-9cae-9cee66c9a892',
+          name: 'book',
+        },
+        permanentLoanType: {
+          id: '2b94c631-fca9-4892-a730-03ee529ffe27',
+          name: 'Can circulate',
+        },
+        metadata: {
+          createdDate: '2019-04-11T03:34:21.745+0000',
+          createdByUserId: '834408d0-5f11-5278-8945-4508aadf94b6',
+          updatedDate: '2019-04-11T12:01:48.451+0000',
+          updatedByUserId: '834408d0-5f11-5278-8945-4508aadf94b6',
+        },
+        effectiveLocation: {
+          id: 'fcd64ce1-6995-48f0-840e-89ffa2288371',
+          name: 'Main Library',
+        },
+      },
+    ],
+    totalRecords: 1,
   });
 
   this.get('/circulation/loans', ({ loans }, request) => {
@@ -253,60 +296,5 @@ export default function configure() {
   this.get('/loan-types', {
     loantypes: [],
     totalRecords: 0
-  });
-
-  this.get('/inventory/items', {
-    items: [
-      {
-        id: '7abd8dfe-0234-4256-b2e6-539499999cac',
-        status: {
-          name: 'Checked out',
-        },
-        title: '14 cows for America',
-        hrid: 'it00000168',
-        contributorNames: [
-          {
-            name: 'Deedy, Carmen Agra.',
-          },
-          {
-            name: 'Naiyomah, Wilson Kimeli.',
-          },
-          {
-            name: 'Gonzalez, Thomas',
-          },
-        ],
-        formerIds: [],
-        discoverySuppress: null,
-        holdingsRecordId: '44a21d17-a666-4f34-b24d-644f8ed1537b',
-        barcode: '5860825104574',
-        copyNumbers: [],
-        notes: [],
-        circulationNotes: [],
-        numberOfPieces: '3',
-        yearCaption: [],
-        electronicAccess: [],
-        statisticalCodeIds: [],
-        purchaseOrderLineIdentifier: null,
-        materialType: {
-          id: '1a54b431-2e4f-452d-9cae-9cee66c9a892',
-          name: 'book',
-        },
-        permanentLoanType: {
-          id: '2b94c631-fca9-4892-a730-03ee529ffe27',
-          name: 'Can circulate',
-        },
-        metadata: {
-          createdDate: '2019-04-11T03:34:21.745+0000',
-          createdByUserId: '834408d0-5f11-5278-8945-4508aadf94b6',
-          updatedDate: '2019-04-11T12:01:48.451+0000',
-          updatedByUserId: '834408d0-5f11-5278-8945-4508aadf94b6',
-        },
-        effectiveLocation: {
-          id: 'fcd64ce1-6995-48f0-840e-89ffa2288371',
-          name: 'Main Library',
-        },
-      },
-    ],
-    totalRecords: 1,
   });
 }
