a5920085fec73489e1bc72e7dcdabb511f822ac0
Michal Kuklis <michal.kuklis@gmail.com>
2019-10-29 12:19:47 -0400

    Bring back settings

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import { hot } from 'react-hot-loader';
 import ReactRouterPropTypes from 'react-router-prop-types';
 
 import InstancesRoute from './routes/InstancesRoute';
+import Settings from './settings';
 
 const InventoryRouting = (props) => {
-  const { match: { path } } = props;
+  const { showSettings, match: { path } } = props;
+
+  if (showSettings) {
+    return <Settings {...props} />;
+  }
 
   return (
     <Switch>
@@ -21,6 +27,7 @@ const InventoryRouting = (props) => {
 
 InventoryRouting.propTypes = {
   match: ReactRouterPropTypes.match,
+  showSettings: PropTypes.bool,
 };
 
 export default hot(module)(InventoryRouting);
