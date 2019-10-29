83626f7b868ef2409fafc8151f0fea62833195a5
Michal Kuklis <michal.kuklis@gmail.com>
2019-10-29 12:25:13 -0400

    Cleanup

--- src/components/HoldingFilters/HoldingFilters.js
+++ src/components/HoldingFilters/HoldingFilters.js
@@ -1,14 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 export default class InstanceFilters extends React.Component {
-  static propTypes = {
-    activeFilters: PropTypes.objectOf(PropTypes.array),
-    onChange: PropTypes.func.isRequired,
-    onClear: PropTypes.func.isRequired,
-    data: PropTypes.object,
-  };
-
   render() {
     return (
       <React.Fragment>
