d293de4815bfebbeddf65bc5466d984e6f092666
Anna_Melnyk <Anna_Melnyk@epam.com>
2019-11-04 15:53:07 +0200

    fix lint errors

--- test/bigtest/interactors/routes/inventory-settings-route.js
+++ test/bigtest/interactors/routes/inventory-settings-route.js
@@ -1,10 +1,6 @@
-import {
-    interactor,
-    isPresent
-  } from '@bigtest/interactor';
-  
+import { interactor, isPresent } from '@bigtest/interactor';
+
 export default @interactor class InventorySettingsRouteInteractor {
-    static defaultScope = '[data-test-inventory-settings]';
-    hasSectionItem = isPresent('[class^=NavListItem---]');
+  static defaultScope = '[data-test-inventory-settings]';
+  hasSectionItem = isPresent('[class^=NavListItem---]');
 }
-  
--- test/bigtest/tests/mirage/routes/settings-inventory-route-test.js
+++ test/bigtest/tests/mirage/routes/settings-inventory-route-test.js
@@ -3,29 +3,30 @@ import { beforeEach, describe, it } from '@bigtest/mocha';
 import { expect } from 'chai';
 
 import setupApplication from '../../../helpers/setup-application';
+
 import InventorySettingsRouteInteractor from '../../../interactors/routes/inventory-settings-route';
 
 describe('InventorySettingsRoute', () => {
-    describe('User has not permissions to view settings/inventory instances, items, holdings', () => {
-        setupApplication({
-            hasAllPerms: false,
-            permissions: {
-                'settings.inventory.enabled': true,
-                'ui-inventory.settings.list.view': false
-              }
-        });
-        const inventorySettingsRoute = new InventorySettingsRouteInteractor();
-
-        beforeEach(async function () {
-            await this.visit('settings/inventory');
-        });
-
-        it('opens settings inventory route', () => {
-            expect(inventorySettingsRoute.isPresent).to.equal(false);
-        });
-
-        it('settings inventory route has inventory instances, items, holdings', () => {
-            expect(inventorySettingsRoute.hasSectionItem).to.be.false;
-        });
+  describe('User has not permissions to view settings/inventory instances, items, holdings', () => {
+    setupApplication({
+      hasAllPerms: false,
+      permissions: {
+        'settings.inventory.enabled': true,
+        'ui-inventory.settings.list.view': false
+      }
     });
+    const inventorySettingsRoute = new InventorySettingsRouteInteractor();
+
+    beforeEach(async function () {
+      await this.visit('settings/inventory');
+    });
+
+    it('opens settings inventory route', () => {
+      expect(inventorySettingsRoute.isPresent).to.equal(false);
+    });
+
+    it('settings inventory route has inventory instances, items, holdings', () => {
+      expect(inventorySettingsRoute.hasSectionItem).to.be.false;
+    });
+  });
 });
