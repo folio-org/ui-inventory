714c1206bb6ca90e9dad08f74149fae8e737db3a
Anna_Melnyk <Anna_Melnyk@epam.com>
2019-11-05 12:45:37 +0200

    add tests

--- test/bigtest/interactors/routes/inventory-settings-route.js
+++
@@ -1,8 +0,0 @@
-import { interactor, isPresent } from '@bigtest/interactor';
-
-export default @interactor class InvSettingsRouteInteractor {
-  static defaultScope = '[data-test-inventory-settings]';
-  hasSectionItem = isPresent('[class^=NavListItem---]');
-  hasInstance = isPresent('#ui-inventory.instances');
-  hasAlternativeTitleInstance = isPresent('#alternative-title-types');
-}
