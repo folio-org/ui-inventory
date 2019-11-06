2c9e44bcd57d4e544d58dd4ca849a7325d2aee6b
Michal Kuklis <michal.kuklis@gmail.com>
2019-11-05 22:27:05 -0500

    Cleanup

--- test/bigtest/network/serializers/material-type.js
+++ test/bigtest/network/serializers/material-type.js
@@ -1,18 +1,12 @@
 import ApplicationSerializer from './application';
 
-const { isArray } = Array;
-
 export default ApplicationSerializer.extend({
   serialize(...args) {
-    const json = ApplicationSerializer.prototype.serialize.apply(this, args);
+    const { materialTypes } = ApplicationSerializer.prototype.serialize.apply(this, args);
 
-    if (isArray(json.materialTypes)) {
-      return {
-        mtypes: json.materialTypes,
-        totalRecords: json.materialTypes.length
-      };
-    } else {
-      return json.instanceStatus;
-    }
+    return {
+      mtypes: materialTypes,
+      totalRecords: materialTypes.length,
+    };
   }
 });
