07861fa204c95a5c1162b4c984b6e393f400f38b
Michal Kuklis <michal.kuklis@gmail.com>
2019-11-11 15:28:11 -0500

    Make SonarCloud happy

--- test/bigtest/tests/filters/instances/search-field-filter-test.js
+++ test/bigtest/tests/filters/instances/search-field-filter-test.js
@@ -20,11 +20,39 @@ describe('SearchFieldFilter', () => {
   describe('Query Search', () => {
     beforeEach(async () => {
       await instancesRoute.searchFieldFilter.searchField.selectIndex('Query search');
-      await instancesRoute.searchFieldFilter.searchField.fillInput('title="Sapiens: A Brief History of Humankind" and contributors =/@name "Yuval Noah Harari"');
+    });
+
+    describe('search without sortby', () => {
+      beforeEach(async () => {
+        await instancesRoute.searchFieldFilter.searchField.fillInput('title="Sapiens: A Brief History of Humankind" and contributors =/@name "Yuval Noah Harari"');
+        await instancesRoute.searchFieldFilter.clickSearch();
+      });
+
+      it('finds instances by title and contributor name', () => {
+        expect(instancesRoute.rows().length).to.equal(1);
+      });
+    });
+
+    describe('search with sortby', () => {
+      beforeEach(async () => {
+        await instancesRoute.searchFieldFilter.searchField.fillInput('title="Sapiens: A Brief History of Humankind" and contributors =/@name "Yuval Noah Harari" sortby title');
+        await instancesRoute.searchFieldFilter.clickSearch();
+      });
+
+      it('finds instances by title and contributor name', () => {
+        expect(instancesRoute.rows().length).to.equal(1);
+      });
+    });
+  });
+
+  describe('ISBN', function () {
+    beforeEach(async () => {
+      await instancesRoute.searchFieldFilter.searchField.selectIndex('- ISBN');
+      await instancesRoute.searchFieldFilter.searchField.fillInput('isbn');
       await instancesRoute.searchFieldFilter.clickSearch();
     });
 
-    it('finds instances by title and contributor name', () => {
+    it('finds instances by isbn', () => {
       expect(instancesRoute.rows().length).to.equal(1);
     });
   });
