afc5974c70087f0c9a1abda4fbe552f6daa4f5eb
Matthew Connolly <mjc12@cornell.edu>
2019-11-12 16:26:38 -0500

    Remove filter inheritance
    Michal and I couldn't come up with a way to make this work to BigTest's satisfaction,
    so taking it out for now is the most expedient approach.

--- test/bigtest/interactors/filters/multiselect-filter.js
+++ test/bigtest/interactors/filters/multiselect-filter.js
@@ -1,9 +1,26 @@
-import { scoped } from '@bigtest/interactor';
+import {
+  clickable,
+  scoped,
+  text,
+  hasClass
+} from '@bigtest/interactor';
 
+import {
+  labelArea,
+  content,
+  expanded,
+} from '@folio/stripes-components/lib/Accordion/Accordion.css';
 // eslint-disable-next-line
 import MultiSelectInteractor from '@folio/stripes-components/lib/MultiSelection/tests/interactor';
-import filterInteractor from './filter';
 
-export default @filterInteractor class MultiSelectFilterInteractor {
+export default class MultiSelectFilterInteractor {
+  static defaultScope = '[data-test-accordion-section]';
+
+  label = text(`.${labelArea}`);
+  isOpen = hasClass(`.${content}`, expanded);
+
+  clear = clickable('button[icon="times-circle-solid"]');
+  open = clickable('[id*="accordion-toggle-button-"]');
+
   multiSelect = scoped('[class^= "multiSelectContainer---"]', MultiSelectInteractor);
 }
