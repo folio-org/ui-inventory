8f28502534d1742d3ba61486bee324afdd0eeeb6
Yuliia-Dovhal <Yuliia_Dovhal@epam.com>
2020-03-13 13:38:20 +0200

    UIDEXP-33: Change translation title to fix typo

--- src/components/InstancesList/InstancesList.js
+++ src/components/InstancesList/InstancesList.js
@@ -230,7 +230,7 @@ class InstancesView extends React.Component {
             size="medium"
             iconClassName={css.actionIcon}
           />
-          <FormattedMessage id="ui-inventory.exportInstancesMARC" />
+          <FormattedMessage id="ui-inventory.exportInstancesMrc" />
         </Button>
         <Button
           disabled
@@ -242,7 +242,7 @@ class InstancesView extends React.Component {
             size="medium"
             iconClassName={css.actionIcon}
           />
-          <FormattedMessage id="ui-inventory.exportInstancesJSON" />
+          <FormattedMessage id="ui-inventory.exportInstancesJson" />
         </Button>
       </Fragment>
     );
--- test/bigtest/tests/export-to-csv-test.js
+++ test/bigtest/tests/export-to-csv-test.js
@@ -65,27 +65,27 @@ describe('Instances', () => {
     });
 
     it('should display action button for export instances (MARC)', () => {
-      expect(inventory.headerDropdownMenu.exportInstancesMARCBtnIsVisible).to.be.true;
+      expect(inventory.headerDropdownMenu.exportInstancesMrcBtnIsVisible).to.be.true;
     });
 
     it('should display correct icon for export instances (MARC)', () => {
-      expect(inventory.headerDropdownMenu.isExportInstancesMARCIconPresent).to.be.true;
+      expect(inventory.headerDropdownMenu.isExportInstancesMrcIconPresent).to.be.true;
     });
 
     it('should disable action button for export instances (MARC) if there are not items in search result', () => {
-      expect(inventory.headerDropdownMenu.isExportInstancesMARCBtnDisabled).to.be.true;
+      expect(inventory.headerDropdownMenu.isExportInstancesMrcBtnDisabled).to.be.true;
     });
 
     it('should display action button for export instances (JSON)', () => {
-      expect(inventory.headerDropdownMenu.exportInstancesJSONBtnIsVisible).to.be.true;
+      expect(inventory.headerDropdownMenu.exportInstancesJsonBtnIsVisible).to.be.true;
     });
 
     it('should display correct icon for export instances (JSON)', () => {
-      expect(inventory.headerDropdownMenu.isExportInstancesJSONIconPresent).to.be.true;
+      expect(inventory.headerDropdownMenu.isExportInstancesJsonIconPresent).to.be.true;
     });
 
     it('should disable action button for export instances (JSON) if there are not items in search result', () => {
-      expect(inventory.headerDropdownMenu.isExportInstancesJSONBtnDisabled).to.be.true;
+      expect(inventory.headerDropdownMenu.isExportInstancesJsonBtnDisabled).to.be.true;
     });
 
     describe('clicking Items in transit report button', () => {
