15e385572991c5fee0cef9c067c2ce0ca1cd10c2
Michał Kuklis <michal.kuklis@gmail.com>
2020-04-16 09:40:46 -0400

    UIIN-817: Add ability to mark item as withdrawn (#963)

--- test/bigtest/network/config.js
+++ test/bigtest/network/config.js
@@ -384,6 +384,14 @@ export default function configure() {
     return item.attrs;
   });
 
+  this.put('/inventory/items/:id/mark-withdrawn', ({ items }, request) => {
+    const item = items.find(request.params.id);
+
+    item.update({ status: { name: 'Withdrawn' } });
+
+    return item.attrs;
+  });
+
   this.post('/inventory/items/:id', ({ items }, request) => {
     const body = JSON.parse(request.requestBody);
     const item = items.create(body);
