6cab1713e80a89d384d9f3ec054ca0db6da92213
Matthew Connolly <mjc12@cornell.edu>
2020-06-26 17:37:40 -0400

    Use new language interface for record display (UIIN-829)

--- src/Instance/InstanceDetails/InstanceDescriptiveView/utils.js
+++ src/Instance/InstanceDetails/InstanceDescriptiveView/utils.js
@@ -1,8 +1,20 @@
-import languagetable from '../../../data/languages';
+// import languagetable from '../../../data/languages';
+import { useIntl } from 'react-intl';
+
+import {
+  intlPreferredLanguageCode,
+  languageByCode,
+} from '@folio/stripes/components';
 
 // eslint-disable-next-line
 export const formatLanguages = (languages = []) => {
+  const intl = useIntl();
   return languages
-    .map(languageCode => languagetable.languageByCode(languageCode))
+    .map(languageCode => {
+      const intlCode = intlPreferredLanguageCode(languageCode);
+      // If formatDisplayName returns a localized language name, great -- use that.
+      // Otherwise, fall back to the English name.
+      return intl.formatDisplayName(intlCode, { fallback: 'none' }) || languageByCode(languageCode).name;
+    })
     .join(', ');
 };
