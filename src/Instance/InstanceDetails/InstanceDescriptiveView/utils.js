54ba3c9dd8b18e320b3d49633e435af8b1da7803
Matthew Connolly <mjc12@cornell.edu>
2020-07-23 15:57:54 -0400

    Improve handling of language names

--- src/Instance/InstanceDetails/InstanceDescriptiveView/utils.js
+++ src/Instance/InstanceDetails/InstanceDescriptiveView/utils.js
@@ -1,8 +1,8 @@
 import { formattedLanguageName } from '@folio/stripes/components';
 
 // eslint-disable-next-line
-export const formatLanguages = (languages = [], intl) => {
+export const formatLanguages = (languages = [], intl, locale) => {
   return languages
-    .map(languageCode => formattedLanguageName(languageCode, intl))
+    .map(languageCode => formattedLanguageName(languageCode, intl, locale))
     .join(', ');
 };
