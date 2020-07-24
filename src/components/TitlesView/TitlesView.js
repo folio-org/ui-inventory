8e67994bad3234855147de1ca9e2ef40309fe2c4
Vlad Samets <uladislau_samets@epam.com>
2020-07-24 09:37:03 +0300

    smell fix

--- src/components/TitlesView/TitlesView.js
+++ src/components/TitlesView/TitlesView.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
