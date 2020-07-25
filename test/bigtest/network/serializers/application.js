3c053a96ff7552517d4d40a6718439b7d362f586
Taras Mankovski <tarasm@gmail.com>
2020-07-25 18:58:51 -0400

    Use Serializer and explicitely state whats included (#1120)
    * Use Serializer and explicitely state whats included
    
    * Use serializeIds=always

--- test/bigtest/network/serializers/application.js
+++ test/bigtest/network/serializers/application.js
@@ -1,3 +1,5 @@
-import { RestSerializer } from '@bigtest/mirage';
+import { Serializer } from '@bigtest/mirage';
 
-export default RestSerializer;
+export default Serializer.extend({
+  serializeIds: 'always'
+});
