ee449c6a40b38f13db58320c20ac0f4857ac0280
Viktor Soroka <viktor_soroka@epam.com>
2020-11-25 22:11:20 +0200

    UIIN-1350: Implement records selection on the inventory search screen  (#1232)
    * UIIN-1349: Implement records selection on the inventory search screen
    
    * UIIN-1349: Add bigtest tests
    
    * Update test/bigtest/tests/filters/instances/instances-list-test.js
    
    Co-authored-by: Yelyzaveta Khokhlova <69502158+yelyzavetakhokhlova@users.noreply.github.com>
    
    Co-authored-by: Yelyzaveta Khokhlova <69502158+yelyzavetakhokhlova@users.noreply.github.com>

--- test/bigtest/tests/filters/instances/instances-list-test.js
+++ test/bigtest/tests/filters/instances/instances-list-test.js
@@ -49,5 +49,53 @@ describe('Instances list', () => {
     it('should display unchecked select row checkbox', () => {
       expect(instancesRoute.selectRowCheckboxes(0).isChecked).to.be.false;
     });
+
+    describe('selecting row', () => {
+      beforeEach(async () => {
+        await instancesRoute.selectRowCheckboxes(0).clickInput();
+      });
+
+      it('should have proper list results size', () => {
+        expect(instancesRoute.rows().length).to.equal(1);
+      });
+
+      it('should display checked select row checkbox', () => {
+        expect(instancesRoute.selectRowCheckboxes(0).isChecked).to.be.true;
+      });
+
+      describe('clicking on reset all button and reapplying previous filter', () => {
+        beforeEach(async () => {
+          await inventory.resetAll();
+          await inventory.source.open();
+          await inventory.source.checkboxes.dataOptions(5).click();
+        });
+
+        it('should reset the selected state for the previously selected row', () => {
+          expect(instancesRoute.selectRowCheckboxes(0).isChecked).to.be.false;
+        });
+      });
+
+      describe('applying filters so previously selected items are no longer displayed', () => {
+        beforeEach(async () => {
+          await inventory.source.open();
+          await inventory.source.checkboxes.dataOptions(5).click();
+        });
+
+        it('should have no results', () => {
+          expect(instancesRoute.rows().length).to.equal(0);
+        });
+
+        describe('applying filters so previously selected items is displayed again', () => {
+          beforeEach(async () => {
+            await inventory.source.open();
+            await inventory.source.checkboxes.dataOptions(5).click();
+          });
+
+          it('should preserve the selected state for the previously selected row', () => {
+            expect(instancesRoute.selectRowCheckboxes(0).isChecked).to.be.true;
+          });
+        });
+      });
+    });
   });
 });
