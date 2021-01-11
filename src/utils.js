917455c983e8e3035e3756f2e5b9c0252601b760
Michal Kuklis <michal.kuklis@gmail.com>
2021-01-11 14:05:21 -0500

    Add couple comments to filterItemsBy

--- src/utils.js
+++ src/utils.js
@@ -176,16 +176,25 @@ export const buildOptionalBooleanQuery = name => values => {
   }
 };
 
+// A clojure function which takes the name of the attribute used
+// for filtering purposes and returns a function which can be used as a custom
+// filter function in <MultiSelection>.
+// https://github.com/folio-org/stripes-components/tree/master/lib/MultiSelection#common-props
+
 export function filterItemsBy(name) {
   return (filter, list) => {
     if (!filter) {
       return { renderedItems: list };
     }
 
-    const esFilter = escapeRegExp(filter);
+    // Escaped filter regex used to filter items on the list.
+    const esFilter = new RegExp(escapeRegExp(filter), 'i');
+
+    // Regex used to return filtered items in alphabetic order.
     const regex = new RegExp(`^${esFilter}`, 'i');
+
     const renderedItems = list
-      .filter(item => item[name].match(new RegExp(esFilter, 'i')))
+      .filter(item => item[name].match(esFilter))
       .sort((item1, item2) => {
         const match1 = item1[name].match(regex);
         const match2 = item2[name].match(regex);
