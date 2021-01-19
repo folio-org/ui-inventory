a7d8ba698007c51a3ec8bf97f5be0945c6c81d65
Mike Taylor <mike@indexdata.com>
2021-01-19 11:07:31 +0000

    Using stripes-final-form; DOES NOT WORK
    The error messaging is singularly unhelpful: "no onSubmit function
    provided" (bit I am providing one) and a track-trace that wanders off
    into hyperspace.
    
    I'm just committing this as a checkpoint before trying something else.

--- src/components/ImportRecordModal/ImportRecordModal.js
+++ src/components/ImportRecordModal/ImportRecordModal.js
@@ -1,33 +1,45 @@
-// See ~/git/folio/stripes/ui-users/src/settings/FeeFinesTable/CopyModal.js
-// for an example of a modal containing a form
+// See ~/git/folio/stripes/ui-users/src/components/Accounts/Actions/CancellationModal.js
+// for an example of a modal using final-form
 
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useIntl, FormattedMessage } from 'react-intl';
+import { Field } from 'react-final-form';
+import setFieldData from 'final-form-set-field-data';
+import stripesFinalForm from '@folio/stripes/final-form';
 
 import {
   Modal,
   ModalFooter,
+  TextField,
   Button,
+  Row,
+  Col,
 } from '@folio/stripes/components';
 
-const SelectedRecordsModal = ({
+import css from './modal.css';
+
+const ImportRecordModal = ({
   isOpen,
   currentOclcNumber, // eslint-disable-line no-unused-vars
-  onGo,
-  onCancel,
+  handleSubmit,
+  handleCancel,
+  submitting,
+  // form: { getState },
 }) => {
   const intl = useIntl();
+  // const { values: { externalIdentifier } } = getState();
+  // const submitButtonDisabled = !externalIdentifier || submitting;
 
   const renderFooter = () => (
     <ModalFooter>
       <Button
         buttonStyle="primary"
-        onClick={() => onGo()}
+        onClick={() => handleSubmit()}
       >
         <FormattedMessage id="ui-inventory.copycat.import" />
       </Button>
-      <Button onClick={onCancel}>
+      <Button onClick={handleCancel}>
         <FormattedMessage id="ui-inventory.cancel" />
       </Button>
     </ModalFooter>
@@ -35,23 +47,32 @@ const SelectedRecordsModal = ({
 
   return (
     <Modal
+      id="import-record-modal"
       open={isOpen}
       label={intl.formatMessage({ id: 'ui-inventory.singleRecordImport' })}
       dismissible
       size="small"
       footer={renderFooter()}
-      onClose={onCancel}
+      onClose={handleCancel}
     >
-      This is ImportRecordModal.
+      Hello
     </Modal>
   );
 };
 
-SelectedRecordsModal.propTypes = {
+ImportRecordModal.propTypes = {
+  // Client-code provides these
   isOpen: PropTypes.bool.isRequired,
   currentOclcNumber: PropTypes.string,
-  onGo: PropTypes.func.isRequired,
-  onCancel: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleCancel: PropTypes.func.isRequired,
+  // The final-form HOC provides these
+  form: PropTypes.object.isRequired,
+  submitting: PropTypes.bool,
+
 };
 
-export default SelectedRecordsModal;
+export default stripesFinalForm({
+  subscription: { values: true },
+  mutators: { setFieldData }
+})(ImportRecordModal);
