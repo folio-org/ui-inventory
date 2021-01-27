a869e346c68f83649dde307b203b367f182b7795
Mike Taylor <mike@indexdata.com>
2021-01-27 22:13:00 +0000

    Remove trailing comma

--- src/components/InstancesList/InstancesList.js
+++ src/components/InstancesList/InstancesList.js
@@ -440,7 +440,7 @@ class InstancesList extends React.Component {
   handleImportRecordModalSubmit = (args) => {
     const { externalIdentifier } = args;
 
-    this.setState({ isImportRecordModalOpened: false, });
+    this.setState({ isImportRecordModalOpened: false });
     console.log('handleImportRecordModalSubmit: externalIdentifier =', externalIdentifier); // eslint-disable-line no-console
     this.props.updateLocation({ layer: 'create', xid: externalIdentifier });
   }
