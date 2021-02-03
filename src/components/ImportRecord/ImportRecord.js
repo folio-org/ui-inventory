f3bf18ede334c20199f3bd802f06eed89c3f86f0
Mike Taylor <mike@indexdata.com>
2021-02-03 18:52:15 +0000

    Break loadExternalRecord apart to use success and failure methods

--- src/components/ImportRecord/ImportRecord.js
+++ src/components/ImportRecord/ImportRecord.js
@@ -29,55 +29,63 @@ class ImportRecord extends React.Component {
   }
 
   loadExternalRecord = (xid) => {
-    const { id, intl, updateLocation } = this.props;
-
     this.props.okapiKy('copycat/imports', {
       timeout: 30000,
       method: 'POST',
       json: {
         externalIdentifier: xid,
-        internalIdentifier: id,
+        internalIdentifier: this.props.id,
         profileId: 'f26df83c-aa25-40b6-876e-96852c3d4fd4' // XXX hardwired to UUID in mod-copycat reference data
       },
-    }).then(res => {
-      // No need to check res.ok, as ky throws non-2xx responses
-      res.json().then(json => {
-        updateLocation({
-          _path: `/inventory/view/${json.internalIdentifier}`,
-          layer: undefined,
-          xid: undefined,
-        });
-      });
-      const message = <FormattedMessage
-        id={`ui-inventory.copycat.callout.${id ? 'updated' : 'created'}`}
-        values={{ xid }}
-      />;
-      this.context.sendCallout({ message });
-    }).catch(err => {
+    })
+      .then(res => this.success(xid, res))
+      .catch(err => this.failure(xid, err));
+  }
+
+  success(xid, res) {
+    const { id, updateLocation } = this.props;
+
+    // No need to check res.ok, as ky throws non-2xx responses
+    res.json().then(json => {
       updateLocation({
-        _path: `/inventory${id ? `/view/${id}` : ''}`,
+        _path: `/inventory/view/${json.internalIdentifier}`,
         layer: undefined,
         xid: undefined,
       });
-
-      if (!(err instanceof ky.HTTPError)) {
-        const message = <FormattedMessage id="ui-inventory.copycat.callout.simpleError" values={{ err: err.toString() }} />;
-        this.context.sendCallout({ timeout: 10000, type: 'error', message });
-      } else {
-        const res = err.response;
-        res.text().then(text => {
-          let detail = text;
-          if (res.headers.get('content-type') === 'application/json') {
-            const obj = JSON.parse(text);
-            detail = obj.errors[0].message;
-          }
-
-          const message = intl.formatMessage({ id: 'ui-inventory.copycat.callout.complexError' }, { err: err.toString(), detail });
-          this.props.stripes.logger.log('action', message);
-          this.context.sendCallout({ timeout: 10000, type: 'error', message });
-        });
-      }
     });
+    const message = <FormattedMessage
+      id={`ui-inventory.copycat.callout.${id ? 'updated' : 'created'}`}
+      values={{ xid }}
+    />;
+    this.context.sendCallout({ message });
+  }
+
+  failure(xid, err) {
+    const { id, updateLocation, intl } = this.props;
+
+    updateLocation({
+      _path: `/inventory${id ? `/view/${id}` : ''}`,
+      layer: undefined,
+      xid: undefined,
+    });
+
+    if (!(err instanceof ky.HTTPError)) {
+      const message = <FormattedMessage id="ui-inventory.copycat.callout.simpleError" values={{ err: err.toString() }} />;
+      this.context.sendCallout({ timeout: 10000, type: 'error', message });
+    } else {
+      const res = err.response;
+      res.text().then(text => {
+        let detail = text;
+        if (res.headers.get('content-type') === 'application/json') {
+          const obj = JSON.parse(text);
+          detail = obj.errors[0].message;
+        }
+
+        const message = intl.formatMessage({ id: 'ui-inventory.copycat.callout.complexError' }, { err: err.toString(), detail });
+        this.props.stripes.logger.log('action', message);
+        this.context.sendCallout({ timeout: 10000, type: 'error', message });
+      });
+    }
   }
 
   render() {
