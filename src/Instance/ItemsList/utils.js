7837498876c1631947f6bbc85101447ae8a079f3
Zak Burke <zburke@cornell.edu>
2021-02-09 21:45:21 -0500

    reimplement sorter functions
    I don't love this, but it does what I want now.

--- src/Instance/ItemsList/utils.js
+++ src/Instance/ItemsList/utils.js
@@ -1,35 +1,53 @@
-import {
-  orderBy,
-} from 'lodash';
-
 /**
  * enumerationSorter
- * Parse an object's enumeration value as a number.
+ * Parse an object's enumeration value numerically
  * Given { enumeration: [string] value } where value is a string like
- * "v.23" or "v.23:no.7-12", return a number like 23 or 23.7, respectively.
- * If the value does not match the pattern, return it as a string.
+ * "v.23" or "v.23:no.11-12", compare "23" and "11".
+ * If the value does not match the pattern, compare the full string.
  *
  * @param  {[object]} an object containing the property "enumeration".
- * @return {[Number|String]} the input object's enumeration property, parse as a number, if possible.
  */
-const enumerationSorter = (i) => {
-  const matches = i.enumeration.match(/^v\.([0-9]+)(:no\.([0-9]).*)?$/);
-  return matches ? Number.parseFloat(`${matches[1]}.${matches[3]}`, 10) : i.enumeration;
+const enumerationSorter = (a, b) => {
+  const aMatches = a.enumeration.match(/^v\.([0-9]+)(:no\.([0-9]+).*)?$/);
+  const bMatches = b.enumeration.match(/^v\.([0-9]+)(:no\.([0-9]+).*)?$/);
+  if (aMatches && bMatches) {
+    if (aMatches[1] === bMatches[1]) {
+      return Number.parseFloat(aMatches[3], 10) - Number.parseFloat(bMatches[3], 10);
+    } else {
+      return Number.parseFloat(aMatches[1], 10) - Number.parseFloat(bMatches[1], 10);
+    }
+  }
+
+  return a.enumeration.localeCompare(b.enumeration);
+};
+
+/**
+ * [loanTypeSorter description]
+ * @param  {[type]} a [description]
+ * @param  {[type]} b [description]
+ * @return {[type]}   [description]
+ */
+const loanTypeSorter = (a, b) => {
+  const aStr = a.temporaryLoanType?.name?.toLowerCase() || a.permanentLoanType?.name?.toLowerCase();
+  const bStr = b.temporaryLoanType?.name?.toLowerCase() || b.permanentLoanType?.name?.toLowerCase();
+  return aStr.localeCompare(bStr);
 };
 
 const sorters = {
-  'barcode': ({ barcode }) => barcode,
-  'status': ({ status }) => status.name.toLowerCase(),
-  'copyNumber': ({ copyNumber }) => copyNumber?.toLowerCase(),
-  'materialType': ({ materialType }) => materialType.name.toLowerCase(),
-  'loanType': (item) => item.temporaryLoanType?.name?.toLowerCase() || item.permanentLoanType?.name?.toLowerCase(),
-  'effectiveLocation': ({ effectiveLocation }) => effectiveLocation.name.toLowerCase(),
+  'barcode': (a, b) => a.barcode.localeCompare(b.barcode),
+  'status': (a, b) => a.status.name.toLowerCase().localeCompare(b.status.name.toLowerCase()),
+  'copyNumber': (a, b) => a.copyNumber?.toLowerCase().localeCompare(b.copyNumber.name.toLowerCase()),
+  'materialType': (a, b) => a.materialType.name.toLowerCase().localeCompare(b.materialType.name.toLowerCase()),
+  'loanType': loanTypeSorter,
+  'effectiveLocation': (a, b) => a.effectiveLocation.name.toLowerCase().localeCompare(b.effectiveLocation.name.toLowerCase()),
   'enumeration': enumerationSorter,
-  'chronology': ({ chronology }) => chronology,
-  'volume': ({ volume }) => volume,
-  'yearCaption': ({ yearCaption }) => yearCaption,
+  'chronology': (a, b) => a.chronology.localeCompare(b.chronology),
+  'volume': (a, b) => a.volume.localeCompare(b.volume),
+  'yearCaption': (a, b) => a.yearCaption.localeCompare(b.yearCaption),
 };
 
-export const sortItems = (items, sorting) => orderBy(
-  items, sorters[sorting.column], sorting.isDesc ? 'desc' : 'asc',
-);
+// eslint-disable-next-line
+export const sortItems = (items, sorting) => {
+  const sorted = [...items].sort(sorters[sorting.column]);
+  return sorting.isDesc ? sorted.reverse() : sorted;
+};
--- src/Instance/ItemsList/utils.test.js
+++ src/Instance/ItemsList/utils.test.js
@@ -1,6 +1,5 @@
 import { sortItems } from './utils';
 
-
 describe('item sort handlers', () => {
   it('should sort by barcode, ascending', () => {
     const list = [
@@ -14,8 +13,7 @@ describe('item sort handlers', () => {
       { barcode: 'b619', enumeration: 'v.200:no.36' },
     ];
 
-    const shuffled = [...list];
-    shuffled.sort(() => Math.random() - 0.5);
+    const shuffled = [...list].sort(() => Math.random() - 0.5);
 
     expect(list).toEqual(sortItems(shuffled, { column: 'barcode' }));
   });
@@ -32,8 +30,7 @@ describe('item sort handlers', () => {
       { barcode: 'b612', enumeration: 'v.1:no.1-6' },
     ];
 
-    const shuffled = [...list];
-    shuffled.sort(() => Math.random() - 0.5);
+    const shuffled = [...list].sort(() => Math.random() - 0.5);
 
     expect(list).toEqual(sortItems(shuffled, { column: 'barcode', isDesc: true }));
   });
@@ -43,16 +40,18 @@ describe('item sort handlers', () => {
     const list = [
       { barcode: 'b612', enumeration: 'v.1:no.1-6' },
       { barcode: 'b613', enumeration: 'v.1:no.2' },
+      { barcode: 'b613', enumeration: 'v.1:no.11' },
       { barcode: 'b614', enumeration: 'v.2:no.1-6' },
       { barcode: 'b615', enumeration: 'v.12:no.1-6' },
       { barcode: 'b616', enumeration: 'v.20:no.1-6' },
       { barcode: 'b617', enumeration: 'v.200:no.1-6' },
-      { barcode: 'b618', enumeration: 'v.200:no.2-12' },
+      { barcode: 'b618', enumeration: 'v.200:no.4-12' },
+      { barcode: 'b618', enumeration: 'v.200:no.5-12' },
+      { barcode: 'b618', enumeration: 'v.200:no.22' },
       { barcode: 'b619', enumeration: 'v.200:no.36' },
     ];
 
-    const shuffled = [...list];
-    shuffled.sort(() => Math.random() - 0.5);
+    const shuffled = [...list].sort(() => Math.random() - 0.5);
 
     expect(list).toEqual(sortItems(shuffled, { column: 'enumeration' }));
   });
@@ -60,17 +59,19 @@ describe('item sort handlers', () => {
   it('should sort by enumeration, numerically descending', () => {
     const list = [
       { barcode: 'b619', enumeration: 'v.200:no.36' },
-      { barcode: 'b618', enumeration: 'v.200:no.2-12' },
+      { barcode: 'b618', enumeration: 'v.200:no.22' },
+      { barcode: 'b618', enumeration: 'v.200:no.5-12' },
+      { barcode: 'b618', enumeration: 'v.200:no.4-12' },
       { barcode: 'b617', enumeration: 'v.200:no.1-6' },
       { barcode: 'b616', enumeration: 'v.20:no.1-6' },
       { barcode: 'b615', enumeration: 'v.12:no.1-6' },
       { barcode: 'b614', enumeration: 'v.2:no.1-6' },
+      { barcode: 'b613', enumeration: 'v.1:no.11' },
       { barcode: 'b613', enumeration: 'v.1:no.2' },
       { barcode: 'b612', enumeration: 'v.1:no.1-6' },
     ];
 
-    const shuffled = [...list];
-    shuffled.sort(() => Math.random() - 0.5);
+    const shuffled = [...list].sort(() => Math.random() - 0.5);
 
     expect(list).toEqual(sortItems(shuffled, { column: 'enumeration', isDesc: true }));
   });
