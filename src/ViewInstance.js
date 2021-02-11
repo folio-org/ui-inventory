4ba628ebe6dd08497d7e9216818be723a063e98d
Denys Bohdan <Denys_Bohdan@epam.com>
2021-02-11 12:23:35 +0200

    UIIN-1407 Added tests

--- src/ViewInstance.js
+++ src/ViewInstance.js
@@ -414,7 +414,7 @@ class ViewInstance extends React.Component {
                 <Button
                   id="duplicate-instance-marc"
                   buttonStyle="dropdownItem"
-                  disabled={!marcRecord} // enable this button when https://issues.folio.org/browse/UIQM-66 is done
+                  disabled // enable this button when https://issues.folio.org/browse/UIQM-66 is done
                   onClick={() => {
                     onToggle();
                     this.duplicateInstanceMarc();
