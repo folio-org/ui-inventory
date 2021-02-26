714b218bf12beb0b4fe6af6a76fae456bf9558d2
Alex Mozolev <aleksey_mozolev@epam.com>
2021-02-26 14:29:21 +0300

    mocha: { timeout: 100000 },

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,11 @@
 /** @param {import('karma').Config} config */
 module.exports = config => config.set({
-  client: { captureConsole: false },
+  client: {
+    captureConsole: false,
+    mocha: {
+      timeout: 100000
+    },
+  },
   browserDisconnectTimeout: 100000,
   browserDisconnectTolerance: 10,
   browserNoActivityTimeout: 100000,
