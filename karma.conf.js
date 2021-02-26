b26a256efce2238c7da01eeb6d222c5ccb2fd78b
Alex Mozolev <aleksey_mozolev@epam.com>
2021-02-26 13:58:12 +0300

    browserDisconnectTimeout: 100000,   browserDisconnectTolerance: 10,   browserNoActivityTimeout: 100000,   flags: [     '--disable-gpu',     '--no-sandbox'   ],

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,11 @@
 /** @param {import('karma').Config} config */
 module.exports = config => config.set({
   client: { captureConsole: false },
-  browserDisconnectTimeout: 600000,
+  browserDisconnectTimeout: 100000,
+  browserDisconnectTolerance: 10,
+  browserNoActivityTimeout: 100000,
+  flags: [
+    '--disable-gpu',
+    '--no-sandbox'
+  ],
 });
