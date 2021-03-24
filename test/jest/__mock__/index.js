b6303ea6f6bd4ee96f0e399d431b95d40ac652ea
Michal Kuklis <michal.kuklis@gmail.com>
2021-03-24 16:25:52 -0400

    Add more tests

--- test/jest/__mock__/index.js
+++ test/jest/__mock__/index.js
@@ -2,6 +2,7 @@ import './currencyData.mock';
 import './documentCreateRange.mock';
 import './matchMedia.mock';
 import './stripesConfig.mock';
+import './stripesConnect.mock';
 import './stripesCore.mock';
 import './stripesIcon.mock';
 import './stripesSmartComponents.mock';
