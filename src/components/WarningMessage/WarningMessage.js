70a76f98614b2e0eb98e077d81e43b19b92e1043
Michał Kuklis <michal.kuklis@gmail.com>
2021-04-09 16:56:10 -0400

    UIIN-1377: Add visual display when instance is suppressed from discovery (#1361)

---
+++ src/components/WarningMessage/WarningMessage.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Icon,
+} from '@folio/stripes/components';
+import {
+  FormattedMessage,
+} from 'react-intl';
+
+import css from './WarningMessage.css';
+
+const renderIcon = (icon, iconClassName) => (
+  <Icon
+    status="warn"
+    iconClassName={iconClassName}
+    icon={icon}
+    iconSize="medium"
+  />
+);
+
+const WarningMessage = ({ id, icon, fill, iconPosition }) => (
+  <div className={fill ? css.fill : ''}>
+    {iconPosition === 'start' && renderIcon(icon, css.iconStart)}
+    <FormattedMessage id={id} />
+    {iconPosition === 'end' && renderIcon(icon, css.iconEnd)}
+  </div>
+);
+
+WarningMessage.propTypes = {
+  id: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+  iconPosition: PropTypes.oneOf(['start', 'end']),
+  fill: PropTypes.bool,
+};
+
+WarningMessage.defaultProps = {
+  icon: 'exclamation-circle',
+  iconPosition: 'start',
+  fill: false,
+};
+
+
+
+export default WarningMessage;
+
