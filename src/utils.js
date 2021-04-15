56066692ac847963931883b8fb1adad49e894e3c
Michal Kuklis <michal.kuklis@gmail.com>
2021-04-15 13:07:11 -0400

    Use FormattedUTCDate

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   FormattedMessage,
   FormattedTime,
-  FormattedDate,
 } from 'react-intl';
 import {
   includes,
@@ -19,6 +18,8 @@ import {
   omit,
 } from 'lodash';
 import moment from 'moment';
+
+import { FormattedUTCDate } from '@folio/stripes/components';
 
 import {
   itemStatusesMap,
@@ -377,12 +378,11 @@ export const convertArrayToBlocks = elements => (!isEmpty(elements)
 
 export const getDate = dateValue => {
   return dateValue ? (
-    <FormattedDate
+    <FormattedUTCDate
       value={dateValue}
       day="numeric"
       month="numeric"
       year="numeric"
-      timeZone="UTC"
     />
   ) : '-';
 };
