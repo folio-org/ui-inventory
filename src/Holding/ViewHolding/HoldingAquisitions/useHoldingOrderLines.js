48e955e6e54da35c5f1f9192126a85a7e3498de5
Mikita Siadykh <mikita_siadykh@epam.com>
2021-05-27 16:48:57 +0400

    UIIN-574 Holdings record. Show connected holding POLs in acq accrodtion. (#1397)

---
+++ src/Holding/ViewHolding/HoldingAquisitions/useHoldingOrderLines.js
@@ -0,0 +1,19 @@
+import { useQuery } from 'react-query';
+
+import { useOkapiKy } from '@folio/stripes/core';
+
+const useHoldingOrderLines = (id, options = {}) => {
+  const ky = useOkapiKy();
+
+  const queryKey = ['ui-inventory', 'holding-lines', id];
+  const queryFn = () => ky.get(`orders/holding-summary/${id}`).json();
+
+  const { data, isLoading } = useQuery({ queryKey, queryFn, ...options });
+
+  return {
+    isLoading,
+    holdingOrderLines: data?.holdingSummaries,
+  };
+};
+
+export default useHoldingOrderLines;
