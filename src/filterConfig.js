056189375918ce0c31d1d028eb67e91c586d10f7
Zak Burke <zburke@cornell.edu>
2021-08-31 11:33:21 -0400

    Revert "UIIN-1567: Incorporate ui-inventory-search facets (#1444)"
    This reverts commit 81140ab13891aad9e5dc3f0efc4d196f2d25cf93.

--- src/filterConfig.js
+++ src/filterConfig.js
@@ -8,86 +8,81 @@ import {
   buildDateRangeQuery,
   buildOptionalBooleanQuery,
 } from './utils';
-import {
-  FACETS,
-  FACETS_CQL
-} from './constants';
-import {
-  AND,
-  NOT,
-  OR
-} from './components/ElasticQueryField/constants';
 
 export const instanceFilterConfig = [
   {
-    name: FACETS.EFFECTIVE_LOCATION,
-    cql: FACETS_CQL.EFFECTIVE_LOCATION,
+    name: 'effectiveLocation',
+    cql: 'item.effectiveLocationId',
     values: [],
   },
   {
-    name: FACETS.LANGUAGE,
-    cql: FACETS_CQL.LANGUAGES,
+    name: 'language',
+    cql: 'languages',
+    values: [],
+    operator: '=',
+  },
+  {
+    name: 'format',
+    cql: 'instanceFormatIds',
+    values: [],
+    operator: '=',
+  },
+  {
+    name: 'resource',
+    cql: 'instanceTypeId',
     values: [],
   },
   {
-    name: FACETS.FORMAT,
-    cql: FACETS_CQL.INSTANCE_FORMAT,
+    name: 'mode',
+    cql: 'modeOfIssuanceId',
     values: [],
   },
   {
-    name: FACETS.RESOURCE,
-    cql: FACETS_CQL.INSTANCE_TYPE,
+    name: 'natureOfContent',
+    cql: 'natureOfContentTermIds',
     values: [],
-  },
-  {
-    name: FACETS.MODE,
-    cql: FACETS_CQL.MODE_OF_ISSUANCE,
-    values: [],
-  },
-  {
-    name: FACETS.NATURE_OF_CONTENT,
-    cql: FACETS_CQL.NATURE_OF_CONTENT,
-    values: [],
+    operator: '=',
   },
   {
     name: 'location',
-    cql: FACETS_CQL.HOLDINGS_PERMANENT_LOCATION,
+    cql: 'holdingsRecords.permanentLocationId',
     values: [],
   },
   {
-    name: FACETS_CQL.STAFF_SUPPRESS,
-    cql: FACETS_CQL.STAFF_SUPPRESS,
+    name: 'staffSuppress',
+    cql: 'staffSuppress',
     values: [],
-    parse: buildOptionalBooleanQuery(FACETS_CQL.STAFF_SUPPRESS),
+    parse: buildOptionalBooleanQuery('staffSuppress'),
   },
   {
-    name: FACETS.INSTANCES_DISCOVERY_SUPPRESS,
-    cql: FACETS_CQL.INSTANCES_DISCOVERY_SUPPRESS,
+    name: 'discoverySuppress',
+    cql: 'discoverySuppress',
     values: [],
-    parse: buildOptionalBooleanQuery(FACETS_CQL.INSTANCES_DISCOVERY_SUPPRESS),
+    parse: buildOptionalBooleanQuery('discoverySuppress'),
   },
   {
-    name: FACETS.CREATED_DATE,
-    cql: FACETS_CQL.CREATED_DATE,
+    name: 'createdDate',
+    cql: 'metadata.createdDate',
     values: [],
-    parse: buildDateRangeQuery(FACETS.CREATED_DATE),
+    parse: buildDateRangeQuery('createdDate'),
   },
   {
-    name: FACETS.UPDATED_DATE,
-    cql: FACETS_CQL.UPDATED_DATE,
+    name: 'updatedDate',
+    cql: 'metadata.updatedDate',
     values: [],
-    parse: buildDateRangeQuery(FACETS.UPDATED_DATE),
+    parse: buildDateRangeQuery('updatedDate'),
   },
   {
-    name: FACETS.SOURCE,
-    cql: FACETS_CQL.SOURCE,
+    name: 'source',
+    cql: 'source',
     operator: '==',
     values: [],
   },
   {
-    name: FACETS.INSTANCES_TAGS,
-    cql: FACETS_CQL.INSTANCES_TAGS,
+    name: 'tags',
+    cql: 'tags.tagList',
     values: [],
+    operator: '=',
   },
 ];
 
@@ -103,24 +98,7 @@ export const instanceIndexes = [
   // { label: 'ui-inventory.barcode', value: 'item.barcode', queryTemplate: 'item.barcode=="%{query.query}"' },
   { label: 'ui-inventory.instanceHrid', value: 'hrid', queryTemplate: 'hrid=="%{query.query}"' },
   { label: 'ui-inventory.instanceId', value: 'id', queryTemplate: 'id="%{query.query}"' },
-  { label: 'ui-inventory.advancedSearch', value: 'advancedSearch', queryTemplate: '%{query.query}' },
-];
-
-export const instanceIndexesES = [
-  { label: 'ui-inventory.search.all', value: 'all', queryTemplate: 'keyword all' },
-  { label: 'ui-inventory.title', value: 'Title', queryTemplate: 'title all' },
-  { label: 'ui-inventory.contributor', value: 'Contributor', queryTemplate: 'contributors=' },
-  { label: 'ui-inventory.identifierAll', value: 'Identifier', queryTemplate: 'identifiers.value==' },
-  { label: 'ui-inventory.issn', value: 'ISSN', queryTemplate: 'issn==' },
-  { label: 'ui-inventory.isbn', value: 'ISBN', queryTemplate: 'isbn==' },
-  { label: 'ui-inventory.subject', value: 'Subject', queryTemplate: 'subjects all' },
-  { label: 'ui-inventory.instanceId', value: 'UUID', queryTemplate: 'id==' },
-  { label: 'ui-inventory.instanceHrid', value: 'HRID', queryTemplate: 'hrid==' },
-  { label: 'ui-inventory.electronicAccessAll', value: 'Electronic access (all)', queryTemplate: 'electronicAccess==' },
-  { label: 'ui-inventory.electronicAccessURI', value: 'Electronic access (URI)', queryTemplate: 'electronicAccess.uri==' },
-  { label: 'ui-inventory.electronicAccessPublicNote', value: 'Electronic access (Public Note)', queryTemplate: 'electronicAccess.publicNote all' },
-  { label: 'ui-inventory.electronicAccessLinkText', value: 'Electronic access (Link text)', queryTemplate: 'electronicAccess.linkText all' },
-  { label: 'ui-inventory.electronicAccessMaterialsSpecified', value: 'Electronic access (Materials specified)', queryTemplate: 'electronicAccess.materialsSpecification all' },
+  { label: 'ui-inventory.querySearch', value: 'querySearch', queryTemplate: '%{query.query}' },
 ];
 
 export const instanceSortMap = {
@@ -145,45 +123,33 @@ export const holdingIndexes = [
     value: 'callNumberNormalized',
     queryTemplate: 'holdingsRecords.fullCallNumberNormalized="%{query.query}" OR holdingsRecords.callNumberAndSuffixNormalized="%{query.query}"' },
   { label: 'ui-inventory.holdingsHrid', value: 'hrid', queryTemplate: 'holdingsRecords.hrid=="%{query.query}"' },
-  { label: 'ui-inventory.advancedSearch', value: 'advancedSearch', queryTemplate: '%{query.query}' },
-];
-
-export const holdingIndexesES = [
-  { label: 'ui-inventory.search.all', value: 'all', queryTemplate: 'keyword all' },
-  { label: 'ui-inventory.issn', value: 'ISSN', queryTemplate: 'issn==' },
-  { label: 'ui-inventory.isbn', value: 'ISBN', queryTemplate: 'isbn==' },
-  { label: 'ui-inventory.callNumber', value: 'Call Number', queryTemplate: 'holdings.fullCallNumber==' },
-  { label: 'ui-inventory.holdingsHrid', value: 'HRID', queryTemplate: 'holdings.hrid==' },
-  { label: 'ui-inventory.electronicAccessAll', value: 'Electronic access (all)', queryTemplate: 'electronicAccess==' },
-  { label: 'ui-inventory.electronicAccessURI', value: 'Electronic access (URI)', queryTemplate: 'electronicAccess.uri==' },
-  { label: 'ui-inventory.electronicAccessPublicNote', value: 'Electronic access (Public Note)', queryTemplate: 'electronicAccess.publicNote all' },
-  { label: 'ui-inventory.electronicAccessLinkText', value: 'Electronic access (Link text)', queryTemplate: 'electronicAccess.linkText all' },
-  { label: 'ui-inventory.electronicAccessMaterialsSpecified', value: 'Electronic access (Materials specified)', queryTemplate: 'electronicAccess.materialsSpecification all' },
+  { label: 'ui-inventory.querySearch', value: 'querySearch', queryTemplate: '%{query.query}' },
 ];
 
 export const holdingSortMap = {};
 
 export const holdingFilterConfig = [
   {
-    name: FACETS.EFFECTIVE_LOCATION,
-    cql: FACETS_CQL.EFFECTIVE_LOCATION,
+    name: 'effectiveLocation',
+    cql: 'item.effectiveLocationId',
     values: [],
   },
   {
-    name: FACETS.HOLDINGS_PERMANENT_LOCATION,
-    cql: FACETS_CQL.HOLDINGS_PERMANENT_LOCATION,
+    name: 'holdingsPermanentLocation',
+    cql: 'holdingsRecords.permanentLocationId',
     values: [],
   },
   {
-    name: FACETS.HOLDINGS_DISCOVERY_SUPPRESS,
-    cql: FACETS_CQL.HOLDINGS_DISCOVERY_SUPPRESS,
+    name: 'discoverySuppress',
+    cql: 'holdingsRecords.discoverySuppress',
     values: [],
-    parse: buildOptionalBooleanQuery(FACETS_CQL.HOLDINGS_DISCOVERY_SUPPRESS),
+    parse: buildOptionalBooleanQuery('holdingsRecords.discoverySuppress'),
   },
   {
-    name: FACETS.HOLDINGS_TAGS,
-    cql: FACETS_CQL.HOLDINGS_TAGS,
+    name: 'tags',
+    cql: 'holdingsRecords.tags.tagList',
     values: [],
+    operator: '=',
   },
 ];
 
@@ -204,66 +170,44 @@ export const itemIndexes = [
     value: 'itemCallNumberNorm',
     queryTemplate: 'item.fullCallNumberNormalized="%{query.query}" OR item.callNumberAndSuffixNormalized="%{query.query}"' },
   { label: 'ui-inventory.itemHrid', value: 'hrid', queryTemplate: 'item.hrid=="%{query.query}"' },
-  { label: 'ui-inventory.advancedSearch', value: 'advancedSearch', queryTemplate: '%{query.query}' },
-];
+  { label: 'ui-inventory.querySearch', value: 'querySearch', queryTemplate: '%{query.query}' },
 
-export const itemIndexesES = [
-  { label: 'ui-inventory.search.all', value: 'all', queryTemplate: 'keyword all' },
-  { label: 'ui-inventory.barcode', value: 'Barcode', queryTemplate: 'items.barcode==' },
-  { label: 'ui-inventory.issn', value: 'ISSN', queryTemplate: 'issn==' },
-  { label: 'ui-inventory.isbn', value: 'ISBN', queryTemplate: 'isbn==' },
-  { label: 'ui-inventory.callNumber', value: 'Call Number', queryTemplate: 'items.effectiveCallNumberComponents==' },
-  { label: 'ui-inventory.itemHrid', value: 'Item HRID', queryTemplate: 'items.hrid==' },
-  { label: 'ui-inventory.electronicAccessAll', value: 'Electronic access (all)', queryTemplate: 'electronicAccess==' },
-  { label: 'ui-inventory.electronicAccessURI', value: 'Electronic access (URI)', queryTemplate: 'electronicAccess.uri==' },
-  { label: 'ui-inventory.electronicAccessPublicNote', value: 'Electronic access (Public Note)', queryTemplate: 'electronicAccess.publicNote all' },
-  { label: 'ui-inventory.electronicAccessLinkText', value: 'Electronic access (Link text)', queryTemplate: 'electronicAccess.linkText all' },
-  { label: 'ui-inventory.electronicAccessMaterialsSpecified', value: 'Electronic access (Materials specified)', queryTemplate: 'electronicAccess.materialsSpecification all' },
 ];
 
 export const itemFilterConfig = [
   {
-    name: FACETS.MATERIAL_TYPE,
-    cql: FACETS_CQL.MATERIAL_TYPES,
+    name: 'materialType',
+    cql: 'item.materialTypeId',
     values: [],
   },
   {
-    name: FACETS.ITEM_STATUS,
-    cql: FACETS_CQL.ITEMS_STATUSES,
+    name: 'itemStatus',
+    cql: 'item.status.name',
     operator: '==',
     values: [],
   },
   {
-    name: FACETS.EFFECTIVE_LOCATION,
-    cql: FACETS_CQL.EFFECTIVE_LOCATION,
+    name: 'effectiveLocation',
+    cql: 'item.effectiveLocationId',
     values: [],
   },
   {
-    name: FACETS.HOLDINGS_PERMANENT_LOCATION,
-    cql: FACETS_CQL.HOLDINGS_PERMANENT_LOCATION,
+    name: 'holdingsPermanentLocation',
+    cql: 'holdingsRecords.permanentLocationId',
     values: [],
   },
   {
-    name: FACETS.ITEMS_DISCOVERY_SUPPRESS,
-    cql: FACETS_CQL.ITEMS_DISCOVERY_SUPPRESS,
+    name: 'discoverySuppress',
+    cql: 'item.discoverySuppress',
     values: [],
-    parse: buildOptionalBooleanQuery(FACETS_CQL.ITEMS_DISCOVERY_SUPPRESS),
+    parse: buildOptionalBooleanQuery('item.discoverySuppress'),
   },
   {
-    name: FACETS.ITEMS_TAGS,
-    cql: FACETS_CQL.ITEMS_TAGS,
+    name: 'tags',
+    cql: 'item.tags.tagList',
     values: [],
+    operator: '=',
   },
-];
-
-const operators = [
-  { label: '=', queryTemplate: '' },
-];
-
-const booleanOperators = [
-  { label: AND },
-  { label: OR },
-  { label: NOT },
 ];
 
 export const itemSortMap = {
@@ -278,27 +222,18 @@ const config = {
     indexes: instanceIndexes,
     sortMap: instanceSortMap,
     renderer: instanceFilterRenderer,
-    indexesES: instanceIndexesES,
-    operators,
-    booleanOperators,
   },
   holdings: {
     filters: holdingFilterConfig,
     indexes: holdingIndexes,
     sortMap: holdingSortMap,
     renderer: holdingsRecordFilterRenderer,
-    indexesES: holdingIndexesES,
-    operators,
-    booleanOperators,
   },
   items: {
     filters: itemFilterConfig,
     indexes: itemIndexes,
     sortMap: itemSortMap,
     renderer: itemFilterRenderer,
-    indexesES: itemIndexesES,
-    operators,
-    booleanOperators,
   }
 };
 
