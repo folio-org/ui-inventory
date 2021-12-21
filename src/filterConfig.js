b17eadeaea4dd591272b0d3e48df721f32532a9d
Vadym Shchekotilin <vadym_shchekotilin@epam.com>
2021-12-21 17:33:33 +0200

    UIIN-1879 Add Browse call numbers option

--- src/filterConfig.js
+++ src/filterConfig.js
@@ -93,6 +93,8 @@ export const instanceIndexes = [
   { label: 'ui-inventory.instanceHrid', value: 'hrid', queryTemplate: 'hrid=="%{query.query}"' },
   { label: 'ui-inventory.instanceId', value: 'id', queryTemplate: 'id="%{query.query}"' },
   { label: 'ui-inventory.querySearch', value: 'querySearch', queryTemplate: '%{query.query}' },
+  { label: 'ui-inventory.browseCallNumbers', value: 'allNumbers', queryTemplate: '%{query.query}' },
+  { label: 'ui-inventory.browseSubjects', value: 'subjects', queryTemplate: '%{query.query}' },
 ];
 
 export const instanceSortMap = {
