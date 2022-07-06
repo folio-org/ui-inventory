8518be11207e127f1fc35f338612ac21e9d596c2
Denys Bohdan <denys_bohdan@epam.com>
2022-07-06 17:04:25 +0300

    UIIN-2079: Browse contributors. Placeholder match does not look right in the contributor column (#1731)
    * UIIN-2079 - Browse contributors. Placeholder match does not look right in the contributor column
    
    * UIIN-2079 - Add overflow visible to cell rendering placeholder
    
    * UIIN-2079 Increased contributors column size, improved missed item styles
    
    Co-authored-by: Priyanka <priyanka_terala@epam.com>
    Co-authored-by: Michał Kuklis <michal.kuklis@gmail.com>

--- src/components/InstancesList/InstancesList.js
+++ src/components/InstancesList/InstancesList.js
@@ -807,9 +807,7 @@ class InstancesList extends React.Component {
             />
           </span>
           <span className={`${css.missingMatchError} ${css.fitContent}`}>
-            &nbsp;
             {query}
-            &nbsp;
           </span>
           <strong className={css.fitContent}>
             <FormattedMessage id="ui-inventory.browseCallNumbers.missedMatch" />
@@ -1062,6 +1060,7 @@ class InstancesList extends React.Component {
             columnWidths={{
               callNumber: '15%',
               subject: '50%',
+              contributor: '50%',
               numberOfTitles: '15%',
               select: '30px',
               title: '40%',
