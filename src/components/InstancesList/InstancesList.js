eac85f4c92fd73efb295839e0e24a292140e0c92
UladzislauKutarkin <72550466+UladzislauKutarkin@users.noreply.github.com>
2022-07-14 17:22:16 +0400

    UIIN-2093:Browse call numbers records found count is non-sensical (#1741)

--- src/components/InstancesList/InstancesList.js
+++ src/components/InstancesList/InstancesList.js
@@ -1049,6 +1049,7 @@ class InstancesList extends React.Component {
             searchableIndexesPlaceholder={null}
             initialResultCount={INITIAL_RESULT_COUNT}
             resultCountIncrement={RESULT_COUNT_INCREMENT}
+            isCountHidden={browseSelectedString}
             viewRecordComponent={ViewInstanceWrapper}
             editRecordComponent={InstanceForm}
             onChangeIndex={onChangeIndex}
