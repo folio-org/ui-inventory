65a468ec519407adca0ca533c8181d09453b1a12
Denys Bohdan <denys_bohdan@epam.com>
2022-07-15 13:03:57 +0300

    UIIN-2106: Add column sizes for contributorType and relatorTerm

--- src/components/InstancesList/InstancesList.js
+++ src/components/InstancesList/InstancesList.js
@@ -1066,6 +1066,8 @@ class InstancesList extends React.Component {
               numberOfTitles: '15%',
               select: '30px',
               title: '40%',
+              contributorType: '15%',
+              relatorTerm: '15%',
             }}
             getCellClass={this.formatCellStyles}
             customPaneSub={this.renderPaneSub()}
