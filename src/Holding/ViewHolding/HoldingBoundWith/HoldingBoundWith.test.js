78c2ddc0af1464598ae33ab579dbeb07f1c0bf4d
Maccabee Levine <msl321@lehigh.edu>
2022-10-13 18:43:07 -0400

    Add tests

---
+++ src/Holding/ViewHolding/HoldingBoundWith/HoldingBoundWith.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { screen } from '@testing-library/react';
+
+import '../../../../test/jest/__mock__';
+import renderWithIntl from '../../../../test/jest/helpers/renderWithIntl';
+
+import {
+  boundWithHoldingsRecords,
+} from './fixtures';
+import HoldingBoundWith from './HoldingBoundWith';
+import useBoundWithHoldings from './useBoundWithHoldings';
+
+jest.mock('./useBoundWithHoldings', () => jest.fn());
+
+const renderHoldingBoundWith = ({
+  boundWithItems = {},
+} = {}) => (
+  renderWithIntl(
+    <Router>
+      <HoldingBoundWith boundWithItems={boundWithItems} />
+    </Router>
+  )
+);
+
+describe('HoldingBoundWith', () => {
+  beforeEach(() => {
+    useBoundWithHoldings.mockClear().mockReturnValue({ boundWithHoldingsRecords });
+  });
+
+  it('should display bound-with fields', () => {
+    renderHoldingBoundWith({ records: [{ hrid: 'BW-ITEM-1' }], hasLoaded: true });
+
+    expect(screen.getByText('ui-inventory.itemHrid')).toBeInTheDocument();
+  });
+});
---
+++ src/Holding/ViewHolding/HoldingBoundWith/fixtures.js
@@ -0,0 +1,7 @@
+// Disabling eslint below because the pattern is to name fixtures
+// eslint-disable-next-line import/prefer-default-export
+export const boundWithHoldingsRecords = [
+  {
+    hrid: 'BW-1',
+  },
+];
