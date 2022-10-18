1e6b1760488e42d29509bc38da35366234cfb3d6
Maccabee Levine <31278545+maccabeelevine@users.noreply.github.com>
2022-10-18 09:45:30 -0400

    UIIN-2212: Link from holdings view bound-with items (#1828)
    * UIIN-2212: Link from holdings view bound-with items
    
    * Fix typo
    
    * Fix query typo
    
    * Allow null records
    
    * Add tests
    
    * Improve efficiency of holdings record lookup
    
    * Use namespace for query
    
    * Fix handling of multiple queryIds

---
+++ src/Holding/ViewHolding/HoldingBoundWith/useBoundWithHoldings.js
@@ -0,0 +1,24 @@
+import { useQuery } from 'react-query';
+
+import { useOkapiKy, useNamespace } from '@folio/stripes/core';
+
+const useBoundWithHoldings = (boundWithItems) => {
+  const ky = useOkapiKy();
+  const [namespace] = useNamespace({ key: 'boundWithHoldings' });
+
+  const holdingRecordIds = boundWithItems.records?.map(x => x.holdingsRecordId);
+  const queryIds = `(${holdingRecordIds.join(' or ')})`;
+
+  const { data, isLoading } = useQuery(
+    [namespace, queryIds],
+    () => ky.get(`holdings-storage/holdings?query=id=${queryIds}`).json(),
+    { enabled: Boolean(queryIds) }
+  );
+
+  return {
+    isLoading,
+    boundWithHoldings: data?.holdingsRecords || [],
+  };
+};
+
+export default useBoundWithHoldings;
