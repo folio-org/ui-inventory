bc89b272fe86e99f35c5fe14d3244673811d0200
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2022-11-28 20:36:22 +0200

    UIIN-2264: Create a new instance search option

--- src/filterConfig.js
+++ src/filterConfig.js
@@ -127,6 +127,7 @@ export const instanceIndexes = [
   { label: 'ui-inventory.effectiveCallNumberShelving', value: 'callNumber', queryTemplate: 'callNumber=%{query.query}' },
   { label: 'ui-inventory.instanceHrid', value: 'hrid', queryTemplate: 'hrid=="%{query.query}"' },
   { label: 'ui-inventory.instanceId', value: 'id', queryTemplate: 'id="%{query.query}"' },
+  { label: 'ui-inventory.authorityId', value: 'authorityId', queryTemplate: 'authorityId == %{query.query}' },
   { label: 'ui-inventory.search.allFields', value: 'allFields', queryTemplate: 'cql.all all "%{query.query}"' },
   { label: 'ui-inventory.querySearch', value: 'querySearch', queryTemplate: '%{query.query}' },
 ];
