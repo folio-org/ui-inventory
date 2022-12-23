bd47afc5fd37427394cf6e114046747892b0e914
ssandupatla <58558770+ssandupatla@users.noreply.github.com>
2022-12-23 18:09:58 +0530

    UIIN-1751 create Jest/RTL test for TargetProfiles.js

---
+++ src/settings/TargetProfiles.test.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
+import '../../test/jest/__mock__';
+
+import {
+  renderWithIntl,
+  stripesStub,
+  translationsProperties
+} from '../../test/jest/helpers';
+
+import TargetProfiles from './TargetProfiles';
+
+jest.unmock('@folio/stripes/components');
+
+
+const resources = {
+  entries: {
+    type: 'okapi',
+    records: [{
+      displayName: '✓test',
+      enabled:true,
+      name:'profiles'
+    }, {
+      displayName: '✕test',
+      enabled:true,
+      name:'profiles'
+    }],
+    path: 'copycat/profiles',
+    resourceShouldRefresh: true,
+    GET: {
+      path: 'copycat/profiles?query=cql.allRecords=1&limit=1000',
+    },
+  }
+};
+
+const mutator = {
+  entries: {
+    PUT: jest.fn(),
+    POST: jest.fn(),
+    DELETE: jest.fn(),
+  }
+};
+
+const defaultProps = {
+  stripes: {
+    ...stripesStub,
+    connect: component => component,
+  },
+  label:'test label',
+  resources,
+  mutator,
+};
+const TargetProfilesSetup = () => (
+  <MemoryRouter>
+    <TargetProfiles {...defaultProps} />
+  </MemoryRouter>
+);
+const renderTargetProfilesSettings = () => renderWithIntl(
+  <TargetProfilesSetup />,
+  translationsProperties
+);
+describe('TargetProfiles Component', () => {
+  it('should take snapshot', () => {
+    const container = renderTargetProfilesSettings();
+    expect(container).toMatchSnapshot();
+  });
+});
