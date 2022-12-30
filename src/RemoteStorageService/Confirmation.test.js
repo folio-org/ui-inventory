79dc494feb5c2af4a943a4397b1ab252dd031afc
KetineniM <mbketineni@ebsco.com>
2022-12-30 14:22:55 +0530

    Review changes updated

--- src/RemoteStorageService/Confirmation.test.js
+++ src/RemoteStorageService/Confirmation.test.js
@@ -1,23 +1,18 @@
-import '../../test/jest/__mock__';
-import { render, screen } from '@testing-library/react';
-import { IntlProvider } from 'react-intl';
+import '../../test/jest/__mock__/intl.mock';
+import { render } from '@testing-library/react';
 import { Heading, Message } from './Confirmation';
 
 describe('Confirmation', () => {
-  it('should render remote confirmation message', () => {
-    const { getByText } = render(
-      <IntlProvider>
-        <Heading />
-      </IntlProvider>
-    );
+  it('should render remote confirmation heading', () => {
+    const { getByText } = render(<Heading />);
     expect(getByText('ui-inventory.remote.confirmation.heading')).toBeInTheDocument();
   });
-  it('should render remote items message', () => {
-    render(
-      <IntlProvider>
-        <Message />
-      </IntlProvider>
-    );
-    expect(screen.getByText('ui-inventory.remote.confirmation.message')).toBeInTheDocument();
+  it('should render remote confirmation message with count props', () => {
+    const { getByText } = render(<Message count={2} />);
+    expect(getByText(/ui-inventory.remote.confirmation.message ui-inventory.remote.items 2/i)).toBeInTheDocument();
+  });
+  it('should render remote confirmation message without count props', () => {
+    const { getByText } = render(<Message />);
+    expect(getByText(/ui-inventory.remote.confirmation.message ui-inventory.remote.items 1/i)).toBeInTheDocument();
   });
 });
