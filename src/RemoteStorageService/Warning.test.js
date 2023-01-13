5c03b2d5ee6596c4b83bdeb050d9a6afa18e050b
ssandupatla <58558770+ssandupatla@users.noreply.github.com>
2023-01-13 19:20:04 +0530

    Update Warning.test.js
    Update review changes in Warning.test.js

--- src/RemoteStorageService/Warning.test.js
+++ src/RemoteStorageService/Warning.test.js
@@ -1,39 +1,24 @@
-import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-
-import '../../test/jest/__mock__';
-
-import { renderWithIntl, translationsProperties } from '../../test/jest/helpers';
+import { render } from '@testing-library/react';
+import '../../test/jest/__mock__/intl.mock';
 import { ForItems, ForHoldings } from './Warning';
 
-const defaultProps = {
-  ForItems: {
-    count: 2,
-  },
-  ForHoldings: {
-    itemCount: 1,
-  },
-};
-
-const ForItemsSetup = () => (
-  <MemoryRouter>
-    <ForItems count={1} />
-    <ForHoldings itemCount={0} />
-  </MemoryRouter>
-);
-
-const renderForItems = () => renderWithIntl(
-  <ForItemsSetup />,
-  translationsProperties
-);
-
 describe('Warning', () => {
-  it('count and itemCount to be defined', () => {
-    renderForItems();
-    expect(defaultProps).toBeDefined();
+  it('should render ForItems with count props ', () => {
+    const { getByText } = render(<ForItems count={2} />);
+    expect(getByText(/ui-inventory.remote.warning.common.items something : ui-inventory.remote.items 2/i)).toBeInTheDocument();
   });
-  it('count and itemCount will through errors if not match', () => {
-    renderForItems();
-    expect(defaultProps).not.toBeFalsy();
+  it('should render ForItems without count props ', () => {
+    const { getByText } = render(<ForItems />);
+    expect(getByText(/ui-inventory.remote.warning.common.items something : ui-inventory.remote.items 1/i)).toBeInTheDocument();
+  });
+  it('should render ForHoldings with itemCount props', () => {
+    const { getByText } = render(<ForHoldings itemCount={1} />);
+    expect(getByText(/ui-inventory.remote.warning.common something : ui-inventory.remote.holdings/i)).toBeInTheDocument();
+    expect(getByText(/ui-inventory.remote.warning.titles count : 1/i)).toBeInTheDocument();
+  });
+  it('should render ForHoldings without itemCount props', () => {
+    const { getByText } = render(<ForHoldings />);
+    expect(getByText(/ui-inventory.remote.warning.common something : ui-inventory.remote.holdings/i)).toBeInTheDocument();
+    expect(getByText(/ui-inventory.remote.warning.titles count : 0/i)).toBeInTheDocument();
   });
 });
