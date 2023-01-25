76f9acc91c8397a94aa526224b9507e33996cb85
cchitneedi <cchitneedi@ebsco.com>
2023-01-25 17:24:12 +0530

    UIIN-1694 create Jest/RTL test for InstancesView.js

--- src/views/InstancesView.test.js
+++ src/views/InstancesView.test.js
@@ -1,43 +1,42 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import '../../test/jest/__mock__';
 import InstancesView from './InstancesView';
+import { InstancesList } from '../components';
 
-jest.mock('../components/InstancesList/InstancesList', () => jest.fn().mockReturnValue('InstancesList'));
-
-const prevProps = {
-  data: {},
-  parentResources: { records: [], facets: [] },
-  segment: '',
-};
-const nextProps = {
-  data: {},
-  parentResources: { records: [], facets: [] },
-  segment: '',
-};
-
-const props = {
-  prevProps,
-  nextProps
-};
-
-const wrapper = ({ children }) => (
-  <MemoryRouter>
-    {children}
-  </MemoryRouter>
-);
-
-const renderInstancesView = () => render(
-  <InstancesView {...props} />,
-  { wrapper },
-);
+jest.mock('../components/InstancesList', () => jest.fn(() => <div>Mocked InstancesList</div>));
 
 describe('InstancesView', () => {
-  it('should render the InstancesList component', () => {
-    renderInstancesView();
-    screen.debug();
-    expect(screen.getByText(/InstancesList/i)).toBeInTheDocument();
-    expect(screen.getByText(/InstancesList/i)).toHaveAttribute('data-test-inventory-instances', 'true');
+  it('InstancesView passes correct props to mocked InstancesList', () => {
+    const data = { id: 1, name: 'Test' };
+    const parentResources = { records: [], facets: {} };
+    render(<InstancesView data={data} parentResources={parentResources} />);
+    expect(InstancesList).toHaveBeenCalledWith({ data, parentResources }, expect.anything());
+  });
+  it('InstancesView re-renders when data prop changes', () => {
+    const data1 = { id: 1, name: 'Test' };
+    const data2 = { id: 2, name: 'Test 2' };
+    const parentResources = { records: [], facets: {} };
+    const { rerender } = render(<InstancesView data={data1} parentResources={parentResources} segment="all" />);
+    act(() => {
+      rerender(<InstancesView data={data2} parentResources={parentResources} segment="all" />);
+    });
+  });
+  it('InstancesView re-renders when parentResources prop changes', () => {
+    const data = { id: 1, name: 'Test' };
+    const parentResources1 = { records: [], facets: {} };
+    const parentResources2 = { records: [{ id:1, name:'Test' }], facets: { name:'test' } };
+    const { rerender } = render(<InstancesView data={data} parentResources={parentResources1} segment="all" />);
+    act(() => {
+      rerender(<InstancesView data={data} parentResources={parentResources2} segment="all" />);
+    });
+  });
+  it('InstancesView re-renders when segment prop changes', () => {
+    const data = { id: 1, name: 'Test' };
+    const parentResources = { records: [], facets: {} };
+    const { rerender } = render(<InstancesView data={data} parentResources={parentResources} segment="all" />);
+    act(() => {
+      rerender(<InstancesView data={data} parentResources={parentResources} segment="active" />);
+    });
   });
 });
