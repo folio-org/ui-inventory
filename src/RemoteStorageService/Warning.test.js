738be95b8ba840d1a2c81d3d1096ad63dd71cf68
ssandupatla <58558770+ssandupatla@users.noreply.github.com>
2023-02-07 15:51:34 +0530

    Update Warning.test.js
    Review changes done

--- src/RemoteStorageService/Warning.test.js
+++ src/RemoteStorageService/Warning.test.js
@@ -1,13 +1,46 @@
 import { render } from '@testing-library/react';
-import '../../test/jest/__mock__/intl.mock';
 import { ForItems, ForHoldings } from './Warning';
 
+jest.mock('react-intl', () => {
+  const intl = {
+    formatMessage: ({ id, values }) => {
+      if (typeof values === 'object') {
+        if (Object.keys(values)[0] === 'something') {
+          if (values.something.props) {
+            if (values.something.props.values) {
+              return `${id} ${Object.keys(values)} : ${(values.something.props.id)} ${Object.values(values.something.props.values)}`;
+            } else {
+              return `${id} ${Object.keys(values)} : ${(values.something.props.id)}`;
+            }
+          }
+        }
+        if (Object.keys(values)[0] === 'count') {
+          return `${id} ${Object.keys(values)} : ${Object.values(values)}`;
+        }
+      }
+      return id;
+    },
+  };
+  return {
+    ...jest.requireActual('react-intl'),
+    FormattedMessage: jest.fn(({ id, values, children }) => {
+      if (children) {
+        return children([intl.formatMessage({ id, values })]);
+      }
+      return intl.formatMessage({ id, values });
+    }),
+    useIntl: () => intl,
+    injectIntl: (Component) => (props) => <Component {...props} intl={intl} />,
+  };
+});
+
+
 describe('Warning', () => {
-  it('should render ForItems with count props ', () => {
+  test('should render ForItems with count props ', () => {
     const { getByText } = render(<ForItems count={2} />);
     expect(getByText(/ui-inventory.remote.warning.common.items something : ui-inventory.remote.items 2/i)).toBeInTheDocument();
   });
-  it('should render ForItems without count props ', () => {
+  test('should render ForItems without count props ', () => {
     const { getByText } = render(<ForItems />);
     expect(getByText(/ui-inventory.remote.warning.common.items something : ui-inventory.remote.items 1/i)).toBeInTheDocument();
   });
