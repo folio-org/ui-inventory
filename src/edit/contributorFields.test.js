9723f50c1c0304b1c8a9f671653e8e886cc08005
VSnehalatha <snehalathav@ebsco.com>
2023-02-08 14:44:30 +0530

    Clearing lint errors

--- src/edit/contributorFields.test.js
+++ src/edit/contributorFields.test.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { screen, fireEvent, waitFor  } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import '../../test/jest/__mock__';
