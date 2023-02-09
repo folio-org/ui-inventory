3447c92954ea06cd63f94e44478b87abbf9c871a
KetineniM <53168935+KetineniM@users.noreply.github.com>
2023-02-09 01:27:12 -0500

    UIIN-1686 JEST/RTL test cases for DataProviders

---
+++ src/providers/DataProviders.test.js
@@ -0,0 +1,546 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '../../test/jest/__mock__';
+
+import DataProvider from './DataProvider';
+
+const Children = () => (
+  <h1>DataProvider</h1>
+);
+
+const resources = {
+  locations: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'location-1',
+        name: 'Location 1',
+      }
+    ],
+  },
+  identifierTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'identifierTypes-1',
+        name: 'IdentifierType 1',
+      }
+    ],
+  },
+  instanceRelationshipTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'instanceRelationshipTypes-1',
+        name: 'InstanceRelationshipType 1',
+      }
+    ],
+  },
+  statisticalCodeTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'statisticalCodeTypes-1',
+        name: 'StatisticalCodeType 1',
+      }
+    ],
+  },
+  contributorTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'contributorTypes-1',
+        contributorTypeId: 'contributorTypes-1',
+      }
+    ],
+  },
+  contributorNameTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'contributorNameTypes-1',
+        name: 'contributorNameTypes 1',
+      }
+    ],
+  },
+  instanceFormats: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'instanceFormats-1',
+        name: 'instanceFormats 1',
+      }
+    ],
+  },
+  instanceTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'instanceTypes-1',
+        name: 'instanceTypes 1',
+      }
+    ],
+  },
+  statisticalCodes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'statisticalCodes-1',
+        statisticalCodeId: 'statisticalCodes-1',
+      }
+    ],
+  },
+  classificationTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'classificationTypes-1',
+        name: 'classificationTypes 1',
+      }
+    ],
+  },
+  alternativeTitleTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'alternativeTitleTypes-1',
+        name: 'alternativeTitleTypes 1',
+      }
+    ],
+  },
+  instanceStatuses: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'instanceStatuses-1',
+        name: 'instanceStatuses 1',
+      }
+    ],
+  },
+  modesOfIssuance: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'modesOfIssuance-1',
+        name: 'modesOfIssuance 1',
+      }
+    ],
+  },
+  instanceNoteTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'instanceNoteTypes-1',
+        name: 'instanceNoteTypes 1',
+      }
+    ],
+  },
+  electronicAccessRelationships: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'electronicAccessRelationships-1',
+        name: 'electronicAccessRelationships 1',
+      }
+    ],
+  },
+  illPolicies: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'illPolicies-1',
+        name: 'illPolicies 1',
+      }
+    ],
+  },
+  holdingsTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'holdingsTypes-1',
+        name: 'holdingsTypes 1',
+      }
+    ],
+  },
+  callNumberTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'callNumberTypes-1',
+        name: 'callNumberTypes 1',
+      }
+    ],
+  },
+  holdingsNoteTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'holdingsNoteTypes-1',
+        name: 'holdingsNoteTypes 1',
+      }
+    ],
+  },
+  itemNoteTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'itemNoteTypes-1',
+        name: 'itemNoteTypes 1',
+      }
+    ],
+  },
+  itemDamagedStatuses: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'itemDamagedStatuses-1',
+        name: 'itemDamagedStatuses 1',
+      }
+    ],
+  },
+  natureOfContentTerms: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'natureOfContentTerms-1',
+        name: 'natureOfContentTerms 1',
+      }
+    ],
+  },
+  materialTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: ' materialTypes-1',
+        name: ' materialTypes 1',
+      }
+    ],
+  },
+  loanTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: ' loanTypes-1',
+        name: ' loanTypes 1',
+      }
+    ],
+  },
+  tags: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'tags-1',
+        name: 'tags 1',
+      }
+    ],
+  },
+  holdingsSources: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'holdingsSources-1',
+        name: 'holdingsSources 1',
+      }
+    ],
+  },
+};
+
+const resources2 = {
+  locations: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'location-1',
+        name: 'Location 1',
+      }
+    ],
+  },
+  identifierTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'identifierTypes-3',
+        name: 'IdentifierType 3',
+      }
+    ],
+  },
+  instanceRelationshipTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'instanceRelationshipTypes-1',
+        name: 'InstanceRelationshipType 1',
+      }
+    ],
+  },
+  statisticalCodeTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'statisticalCodeTypes-1',
+        name: 'StatisticalCodeType 1',
+      }
+    ],
+  },
+  contributorTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'contributorTypes-1',
+        contributorTypeId: 'contributorTypes-1',
+      }
+    ],
+  },
+  contributorNameTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'contributorNameTypes-1',
+        name: 'contributorNameTypes 1',
+      }
+    ],
+  },
+  instanceFormats: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'instanceFormats-1',
+        name: 'instanceFormats 1',
+      }
+    ],
+  },
+  instanceTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'instanceTypes-1',
+        name: 'instanceTypes 1',
+      }
+    ],
+  },
+  statisticalCodes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'statisticalCodes-1',
+        statisticalCodeId: 'statisticalCodes-1',
+      }
+    ],
+  },
+  classificationTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'classificationTypes-1',
+        name: 'classificationTypes 1',
+      }
+    ],
+  },
+  alternativeTitleTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'alternativeTitleTypes-1',
+        name: 'alternativeTitleTypes 1',
+      }
+    ],
+  },
+  instanceStatuses: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'instanceStatuses-1',
+        name: 'instanceStatuses 1',
+      }
+    ],
+  },
+  modesOfIssuance: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'modesOfIssuance-1',
+        name: 'modesOfIssuance 1',
+      }
+    ],
+  },
+  instanceNoteTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'instanceNoteTypes-1',
+        name: 'instanceNoteTypes 1',
+      }
+    ],
+  },
+  electronicAccessRelationships: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'electronicAccessRelationships-1',
+        name: 'electronicAccessRelationships 1',
+      }
+    ],
+  },
+  illPolicies: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'illPolicies-1',
+        name: 'illPolicies 1',
+      }
+    ],
+  },
+  holdingsTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'holdingsTypes-1',
+        name: 'holdingsTypes 1',
+      }
+    ],
+  },
+  callNumberTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'callNumberTypes-1',
+        name: 'callNumberTypes 1',
+      }
+    ],
+  },
+  holdingsNoteTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'holdingsNoteTypes-1',
+        name: 'holdingsNoteTypes 1',
+      }
+    ],
+  },
+  itemNoteTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'itemNoteTypes-1',
+        name: 'itemNoteTypes 1',
+      }
+    ],
+  },
+  itemDamagedStatuses: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'itemDamagedStatuses-1',
+        name: 'itemDamagedStatuses 1',
+      }
+    ],
+  },
+  natureOfContentTerms: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'natureOfContentTerms-1',
+        name: 'natureOfContentTerms 1',
+      }
+    ],
+  },
+  materialTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: ' materialTypes-1',
+        name: ' materialTypes 1',
+      }
+    ],
+  },
+  loanTypes: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: ' loanTypes-1',
+        name: ' loanTypes 1',
+      }
+    ],
+  },
+  tags: {
+    hasLoaded: true,
+    failed: false,
+    records: [
+      {
+        id: 'tags-1',
+        name: 'tags 1',
+      }
+    ],
+  },
+  holdingsSources: {
+    hasLoaded: false,
+    failed: false,
+    records: [
+      {
+        id: 'holdingsSources-1',
+        name: 'holdingsSources 1',
+      }
+    ],
+  },
+};
+
+describe('DataProvider', () => {
+  it('Component should render properly', () => {
+    render(<DataProvider resources={resources}><Children /></DataProvider>);
+    expect(screen.getByText('DataProvider')).toBeInTheDocument();
+  });
+  it('Component should be empty', () => {
+    const { container } = render(<DataProvider resources={resources2}><Children /></DataProvider>);
+    expect(container).toBeEmptyDOMElement();
+  });
+});
