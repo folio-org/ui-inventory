f6cfa6036c01d06bc880682e230c0feb733a1b65
KetineniM <53168935+KetineniM@users.noreply.github.com>
2023-03-10 01:03:52 -0500

    support mock files included

--- test/jest/__mock__/stripesSmartComponents.mock.js
+++ test/jest/__mock__/stripesSmartComponents.mock.js
@@ -5,4 +5,15 @@ jest.mock('@folio/stripes/smart-components', () => ({
   LocationLookup: () => <div>LocationLookup</div>,
   ViewMetaData: () => <div>ViewMetaData</div>,
   ControlledVocab: () => <div>ControlledVocab</div>,
+  ConfigManager: (props) => {
+    const { getInitialValues, onBeforeSave, children } = props;
+    const component =
+      <div>
+        <div>ConfigManager</div>
+        {children}
+        <button type="button" onClick={() => getInitialValues()}>getInitialValues</button>
+        <button type="button" onClick={() => onBeforeSave()}>onBeforeSave</button>
+      </div>;
+    return component;
+  },
 }), { virtual: true });
