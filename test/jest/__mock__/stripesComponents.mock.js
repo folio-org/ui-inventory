14fa6437b8ff88f45ed03754d0b183d00ac3c4fe
KetineniM <53168935+KetineniM@users.noreply.github.com>
2023-03-10 02:59:50 -0500

    StripesComponent file included

--- test/jest/__mock__/stripesComponents.mock.js
+++ test/jest/__mock__/stripesComponents.mock.js
@@ -3,4 +3,5 @@ import React from 'react';
 jest.mock('@folio/stripes/components', () => ({
   ...jest.requireActual('@folio/stripes/components'),
   LoadingView: () => <div>LoadingView</div>,
+  LoadingPane: () => <div>LoadingPane</div>,
 }), { virtual: true });
