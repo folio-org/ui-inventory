696a0ac9633316708bb7a568d8b4f87086aa08e8
cchitneedi <100114810+cchitneedi@users.noreply.github.com>
2023-03-10 17:33:32 +0530

    Update renderWithIntl.js
    renderWithIntl restored

--- test/jest/helpers/renderWithIntl.js
+++ test/jest/helpers/renderWithIntl.js
@@ -3,16 +3,10 @@ import { render } from '@testing-library/react';
 
 import Harness from './Harness';
 
-let rtlApi;
-
-const renderWithIntl = (children, translations = [], options = {}) => {
-  const renderFn = options.rerender ? rtlApi.rerender : render;
-  rtlApi = renderFn(
-    <Harness translations={translations}>
-      {children}
-    </Harness>
-  );
-  return rtlApi;
-};
+const renderWithIntl = (children, translations = [], renderer = render) => renderer(
+  <Harness translations={translations}>
+    {children}
+  </Harness>
+);
 
 export default renderWithIntl;
