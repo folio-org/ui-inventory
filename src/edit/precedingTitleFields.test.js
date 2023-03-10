18f3f91279bb37f7cabd24728e695d3ca9608e54
cchitneedi <100114810+cchitneedi@users.noreply.github.com>
2023-03-10 17:25:01 +0530

    Update precedingTitleFields.test.js
    Test changes

--- src/edit/precedingTitleFields.test.js
+++ src/edit/precedingTitleFields.test.js
@@ -1,20 +1,15 @@
 import PropTypes from 'prop-types';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import '../../test/jest/__mock__';
-
 import stripesFinalForm from '@folio/stripes/final-form';
 import renderWithRouter from '../../test/jest/helpers/renderWithRouter';
 import renderWithIntl from '../../test/jest/helpers/renderWithIntl';
-
-import PrecedingTitles from './precedingTitleFields';
 import translationsProperties from '../../test/jest/helpers/translationsProperties';
+import PrecedingTitles from './precedingTitleFields';
 
 jest.unmock('@folio/stripes/components');
-
-jest.mock('../components', () => ({
-  TitleField: jest.fn(() => 'mocked TitleField'),
-}));
 
 const onSubmit = jest.fn();
 
@@ -44,14 +39,13 @@ afterEach(() => jest.clearAllMocks());
 
 describe('precedingTitleFields', () => {
   it('renders RepeatableField', () => {
-    const { getByText } = renderPrecedingTitles();
-    expect(getByText(/Preceding titles/i)).toBeInTheDocument();
-    expect(getByText(/Add preceding title/i)).toBeInTheDocument();
+    renderPrecedingTitles();
+    expect(screen.getByText(/Preceding titles/i)).toBeInTheDocument();
+    expect(screen.getByText(/Add preceding title/i)).toBeInTheDocument();
   });
   it('click Add preceding title button', () => {
-    const { getByText } = renderPrecedingTitles();
-    const precedingButton = getByText(/Add preceding title/i);
-    userEvent.click(precedingButton);
-    expect(getByText(/mocked TitleField/i)).toBeInTheDocument();
+    renderPrecedingTitles();
+    userEvent.click(screen.getByText(/Add preceding title/i));
+    expect(screen.getAllByText(/Instance HRID/i).length).toBe(1);
   });
 });
