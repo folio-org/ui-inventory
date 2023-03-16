ac05541b598e662b0e4f21c036e6c8dac3a46aeb
VSnehalatha <snehalathav@ebsco.com>
2023-03-16 19:44:20 +0530

    JEST/RTL tests for FastAddForm.js

---
+++ src/settings/FastAdd/FastAddForm.test.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import { FormattedMessage } from 'react-intl';
+import '../../../test/jest/__mock__';
+import {
+  renderWithIntl,
+  translationsProperties,
+} from '../../../test/jest/helpers';
+import FastAddForm from './FastAddForm';
+
+const mockOnSubmit = jest.fn();
+const mockOnCancel = jest.fn();
+const handleSubmit = jest.fn();
+const pristine = true;
+const submitting = true;
+const form = {};
+const label = <FormattedMessage id="ui-inventory" />;
+const FastAddFormSetup = (props = {}) => (
+  <Router>
+    <FastAddForm
+      onSubmit={mockOnSubmit}
+      onCancel={mockOnCancel}
+      handleSubmit={handleSubmit}
+      pristine={pristine}
+      submitting={submitting}
+      form={form}
+      label={label}
+      {...props}
+    >
+      <div>children</div>
+    </FastAddForm>
+  </Router>
+);
+
+const renderFastAddForm = (props = {}) => renderWithIntl(
+  <FastAddFormSetup {...props} />,
+  translationsProperties
+);
+
+describe('FastAddForm', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should render FastAddForm', () => {
+    renderFastAddForm();
+    expect(screen.getByText('ui-inventory')).toBeInTheDocument();
+    expect(screen.getByText('children')).toBeInTheDocument();
+  });
+  test('Cancel button shoul be truthy', async () => {
+    renderFastAddForm();
+    userEvent.click(screen.getByText('Cancel'));
+    expect(screen.getByRole('button', { name: /Cancel/i })).toBeTruthy();
+  });
+  test('Save button shoul be truthy', async () => {
+    renderFastAddForm();
+    const submitButton = screen.getByText(/Save/i);
+    userEvent.click(submitButton);
+    screen.debug();
+    expect(screen.getByRole('button', { name: /Save/i })).toBeTruthy();
+  });
+});
