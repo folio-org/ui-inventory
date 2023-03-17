744b1c7d88cc3735f55bbfa9c6069b609ef7bd07
Srinath Ratnapuram <sratnapuram@corp.epnet.com>
2023-03-17 09:53:41 +0530

    UIIN-1805 create Jest/RTL test for RemoteStorageWarning.js

--- test/jest/__mock__/stripesSmartComponents.mock.js
+++ test/jest/__mock__/stripesSmartComponents.mock.js
@@ -5,4 +5,29 @@ jest.mock('@folio/stripes/smart-components', () => ({
   LocationLookup: () => <div>LocationLookup</div>,
   ViewMetaData: () => <div>ViewMetaData</div>,
   ControlledVocab: () => <div>ControlledVocab</div>,
+  ConfigManager: (props) => {
+    const { getInitialValues, onBeforeSave, children } = props;
+    const component =
+      <div>
+        <div>ConfigManager</div>
+        {children}
+        <button type="button" onClick={() => getInitialValues()}>getInitialValues</button>
+        <button type="button" onClick={() => onBeforeSave()}>onBeforeSave</button>
+      </div>;
+    return component;
+  },
+  useRemoteStorageMappings: () => {
+    return ({
+      'holdings-id-1': {
+        'id': 'holdings-id-1',
+        'name': 'Storage A',
+        'description': 'Storage A description'
+      },
+      'holdings-id-2': {
+        'id': 'holdings-id-2',
+        'name': 'Storage B',
+        'description': 'Storage B description'
+      }
+    });
+  }
 }), { virtual: true });
