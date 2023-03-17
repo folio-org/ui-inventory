f41da009d40e58bf08ef03496a53a096ec40485c
VSnehalatha <snehalathav@ebsco.com>
2023-03-17 10:56:39 +0530

    All branches coverage

--- src/edit/holdings/RemoteStorageWarning.test.js
+++ src/edit/holdings/RemoteStorageWarning.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Form } from 'react-final-form';
 import { screen } from '@testing-library/react';
+import { cleanup } from '@testing-library/react-hooks';
 import '../../../test/jest/__mock__';
 import { renderWithIntl, translationsProperties } from '../../../test/jest/helpers';
 import { RemoteStorageWarning } from './RemoteStorageWarning';
@@ -25,30 +26,40 @@ jest.mock('react-final-form', () => ({
     initialValues,
     values
   })
+    .mockReturnValueOnce({
+      initialValues,
+      values: {
+        permanentLocationId: 'holdings-id-1',
+        temporaryLocationId: 'holdings-id-2',
+      }
+    })
 }));
 
-const renderRemoteStorageWarning = (props) => {
-  const component = () => {
-    return (
-      <RemoteStorageWarning {...props} />
-    );
-  };
-  renderWithIntl(
-    <Form
-      id="form-id"
-      onSubmit={jest.fn()}
-      render={component}
-    />,
-    translationsProperties
+const component = () => {
+  return (
+    <RemoteStorageWarning {...defaultProps} />
   );
 };
 
+const renderRemoteStorageWarning = () => renderWithIntl(
+  <Form
+    id="form-id"
+    onSubmit={jest.fn()}
+    render={component}
+  />,
+  translationsProperties
+);
+
 describe('RemoteStorageWarning', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
+  beforeEach(() => {
+    cleanup();
   });
-  it('Component should render properly', () => {
+  it('DOM to be empty when values are unchanged', () => {
     renderRemoteStorageWarning(defaultProps);
+    expect(screen.getByRole('alert')).toBeEmptyDOMElement();
+  });
+  it('Warning text should render when values change', () => {
+    renderRemoteStorageWarning();
     expect(screen.getByText(/To remove the holdings from remote storage, run an exception report or communicate this directly to your remote storage location. This includes/i)).toBeInTheDocument();
     expect(screen.getByText(1)).toBeInTheDocument();
     expect(screen.getByText(/title./i)).toBeInTheDocument();
