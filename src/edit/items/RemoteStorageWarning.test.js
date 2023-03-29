f786507757786b49363e0cc7af370c5687078545
cchitneedi <100114810+cchitneedi@users.noreply.github.com>
2023-03-29 18:45:02 +0530

    Update RemoteStorageWarning.test.js
    Updated with Class file.

--- src/edit/items/RemoteStorageWarning.test.js
+++ src/edit/items/RemoteStorageWarning.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Form } from 'react-final-form';
-import { screen } from '@testing-library/react';
 import { cleanup } from '@testing-library/react-hooks';
 import '../../../test/jest/__mock__';
 import { renderWithIntl, translationsProperties } from '../../../test/jest/helpers';
@@ -51,11 +50,11 @@ describe('RemoteStorageWarning', () => {
     cleanup();
   });
   it('DOM to be empty when values are unchanged', () => {
-    renderRemoteStorageWarning();
-    expect(screen.getByRole('alert')).toBeEmptyDOMElement();
+    const { getByRole } = renderRemoteStorageWarning();
+    expect(getByRole('alert')).toBeEmptyDOMElement();
   });
   it('Warning text should render when values change', () => {
-    renderRemoteStorageWarning();
-    expect(screen.getByText(/Item has been successfully moved in FOLIO. To complete removing this item from remote storage, run an exception report or communicate this directly to your remote storage location./i)).toBeInTheDocument();
+    const { container } = renderRemoteStorageWarning();
+    expect(container.getElementsByClassName('inner type-warning').length).toBe(1);
   });
 });
