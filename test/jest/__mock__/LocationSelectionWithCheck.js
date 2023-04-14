079914071b291e37578ed1796b5c41a134e2df64
ssandupatla <58558770+ssandupatla@users.noreply.github.com>
2023-04-14 14:53:11 +0530

    Supported files for UIIN-1688

---
+++ test/jest/__mock__/LocationSelectionWithCheck.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { useIntl } from 'react-intl';
+import { Field } from 'react-final-form';
+
+import { LocationLookup, LocationSelection } from '@folio/stripes/smart-components';
+import { ConfirmationModal, MessageBanner } from '@folio/stripes/components';
+
+import * as RemoteStorage from '../../RemoteStorageService';
+
+
+const MODAL_CLOSED = {
+  open: false,
+  heading: '',
+};
+
+
+const LocationSelectionWithCheck = ({ input, ...rest }) => {
+  const { value, onChange, ...restInput } = input;
+
+  const { formatMessage } = useIntl();
+
+  const checkFromRemoteToNonRemote = RemoteStorage.Check.useByLocation();
+
+  const [selectedValue, setSelectedValue] = useState(value);
+  const [rollbackValue, setRollbackValue] = useState(value);
+  const [modal, setModal] = useState(MODAL_CLOSED);
+
+  const fromLocationId = rest.meta.initial;
+
+  const handleSelect = location => {
+    const toLocationId = location?.id;
+
+    setSelectedValue(toLocationId);
+
+    const isNotSameLocation = toLocationId !== value;
+    const isNewLocationInactive = location && !location.isActive;
+    const isMovingFromRemote = checkFromRemoteToNonRemote({ fromLocationId, toLocationId });
+    const isConfirmationNeeded = (isNewLocationInactive || isMovingFromRemote) && isNotSameLocation;
+
+    if (isConfirmationNeeded) {
+      const heading =
+        (isNewLocationInactive && isMovingFromRemote && formatMessage({ id: 'ui-inventory.location.confirm.common.header' }))
+        || (isNewLocationInactive && formatMessage({ id: 'ui-inventory.location.confirm.inactive.header' }))
+        || (isMovingFromRemote && <RemoteStorage.Confirmation.Heading />);
+
+      const messages = [
+        isNewLocationInactive && formatMessage({ id: 'ui-inventory.location.confirm.inactive.message' }),
+        isMovingFromRemote && <RemoteStorage.Confirmation.Message />,
+      ];
+      const message = messages.filter(Boolean).map(m => <MessageBanner type="warning" key={m}>{m}</MessageBanner>);
+
+      setRollbackValue(isMovingFromRemote ? fromLocationId : value);
+      setModal({ open: true, heading, message });
+
+      return;
+    }
+
+    onChange(toLocationId);
+  };
+
+  const handleConfirm = () => {
+    setModal(MODAL_CLOSED);
+    onChange(selectedValue);
+  };
+
+  const handleCancel = () => {
+    setModal(MODAL_CLOSED);
+    setSelectedValue(rollbackValue);
+  };
+
+  return (
+    <>
+      <LocationSelection
+        placeholder={formatMessage({ id: 'ui-inventory.selectLocation' })}
+        value={value}
+        onSelect={handleSelect}
+        {...restInput}
+        {...rest}
+      />
+
+      <LocationLookup
+        onLocationSelected={handleSelect}
+        disabled={rest.disabled}
+      />
+
+      <ConfirmationModal
+        confirmLabel={formatMessage({ id: 'ui-inventory.location.confirm.confirmBtn' })}
+        buttonStyle="default"
+        cancelButtonStyle="primary"
+        bodyTag="div"
+        onConfirm={handleConfirm}
+        onCancel={handleCancel}
+        {...modal}
+      />
+    </>
+  );
+};
+
+LocationSelectionWithCheck.propTypes = Field.propTypes;
+
+export default LocationSelectionWithCheck;
--- test/jest/__mock__/stripesSmartComponents.mock.js
+++ test/jest/__mock__/stripesSmartComponents.mock.js
@@ -3,32 +3,12 @@ import React from 'react';
 jest.mock('@folio/stripes/smart-components', () => ({
   ...jest.requireActual('@folio/stripes/smart-components'),
   LocationLookup: () => <div>LocationLookup</div>,
+  LocationSelection: ({ onSelect }) => (
+    <button data-testid="LocationSelectionSelectBtn" type="button" onClick={onSelect}>
+      Select
+    </button>
+  ),
   ViewMetaData: () => <div>ViewMetaData</div>,
   ControlledVocab: () => <div>ControlledVocab</div>,
-  ConfigManager: (props) => {
-    const { getInitialValues, onBeforeSave, children } = props;
-    const component =
-      <div>
-        <div>ConfigManager</div>
-        {children}
-        <button type="button" onClick={() => getInitialValues()}>getInitialValues</button>
-        <button type="button" onClick={() => onBeforeSave()}>onBeforeSave</button>
-      </div>;
-    return component;
-  },
-  useRemoteStorageMappings: () => {
-    return ({
-      'holdings-id-1': {
-        'id': 'holdings-id-1',
-        'name': 'Storage A',
-        'description': 'Storage A description'
-      },
-      'holdings-id-2': {
-        'id': 'holdings-id-2',
-        'name': 'Storage B',
-        'description': 'Storage B description'
-      }
-    });
-  }
+  // useRemoteStorageMappings: () => <div>useRemoteStorageMappings</div>,
 }), { virtual: true });
-
