97566cd52d41b944c177fa5750929bc32ca054ab
ssandupatla <58558770+ssandupatla@users.noreply.github.com>
2023-04-14 18:25:36 +0530

    Update stripesSmartComponents.mock.js
    Updated

--- test/jest/__mock__/stripesSmartComponents.mock.js
+++ test/jest/__mock__/stripesSmartComponents.mock.js
@@ -2,13 +2,35 @@ import React from 'react';
 
 jest.mock('@folio/stripes/smart-components', () => ({
   ...jest.requireActual('@folio/stripes/smart-components'),
-  LocationLookup: () => <div>LocationLookup</div>,
-  LocationSelection: ({ onSelect }) => (
-    <button data-testid="LocationSelectionSelectBtn" type="button" onClick={onSelect}>
-      Select
-    </button>
-  ),
-  ViewMetaData: () => <div>ViewMetaData</div>,
   ControlledVocab: () => <div>ControlledVocab</div>,
-  // useRemoteStorageMappings: () => <div>useRemoteStorageMappings</div>,
+  ConfigManager: (props) => {
+    const { getInitialValues, onBeforeSave, children } = props;
+    const component =
+      <div>
+        <div>ConfigManager</div>
+        {children}
+        <button type="button" onClick={() => getInitialValues()}>getInitialValues</button>
+        <button type="button" onClick={() => onBeforeSave()}>onBeforeSave</button>
+      </div>;
+    return component;
+  },
+  LocationLookup: () => <div>LocationLookup</div>,
+  LocationSelection: jest.fn(({ onSelect }) => (
+    <button data-testid="LocationSelectionSelectBtn" type="button" onClick={onSelect}>Select</button>
+  )),
+  useRemoteStorageMappings: () => {
+    return ({
+      'holdings-id-1': {
+        'id': 'holdings-id-1',
+        'name': 'Storage A',
+        'description': 'Storage A description'
+      },
+      'holdings-id-2': {
+        'id': 'holdings-id-2',
+        'name': 'Storage B',
+        'description': 'Storage B description'
+      }
+    });
+  },
+  ViewMetaData: () => <div>ViewMetaData</div>,
 }), { virtual: true });
