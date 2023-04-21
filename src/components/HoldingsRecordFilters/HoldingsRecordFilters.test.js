985cd758349010125db37f74bcfa80f86f5f6cd1
cchitneedi <cchitneedi@ebsco.com>
2023-04-21 16:46:00 +0530

    UIIN-1607 create Jest/RTL test for HoldingsRecordFilters.js

--- src/components/HoldingsRecordFilters/HoldingsRecordFilters.test.js
+++ src/components/HoldingsRecordFilters/HoldingsRecordFilters.test.js
@@ -1,19 +1,53 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { noop } from 'lodash';
+import { screen } from '@testing-library/react';
+import { ModuleHierarchyProvider } from '@folio/stripes-core/src/components/ModuleHierarchy';
 
-import '../../../test/jest/__mock__';
-
-import { ModuleHierarchyProvider } from '@folio/stripes/core';
+import '../../../test/jest/__mock__/currencyData.mock';
+import '../../../test/jest/__mock__/stripesConfig.mock';
+import '../../../test/jest/__mock__/stripesConnect.mock';
+import '../../../test/jest/__mock__/stripesCore.mock';
+import '../../../test/jest/__mock__/stripesIcon.mock';
+import userEvent from '@testing-library/user-event';
 import renderWithIntl from '../../../test/jest/helpers/renderWithIntl';
 
 import HoldingsRecordFilters from './HoldingsRecordFilters';
+import translationsProperties from '../../../test/jest/helpers/translationsProperties';
+
+jest.mock('../CheckboxFacet/CheckboxFacet', () => jest.fn().mockReturnValue('CheckboxFacet'));
+
+jest.mock('../../facetUtils', () => ({
+  ...jest.requireActual('../../facetUtils'),
+  getSourceOptions: jest.fn(),
+  getSuppressedOptions: jest.fn(),
+}));
+
+jest.mock('@folio/stripes/components', () => ({
+  ...jest.requireActual('@folio/stripes/components'),
+  Accordion: (props) => {
+    const { onClearFilter, name, children } = props;
+    const component =
+      <div>
+        <button type="button" name={name} onClick={() => onClearFilter()}>{name}</button>
+        <div>{children}</div>
+      </div>;
+    return (component);
+  },
+}));
 
 const resources = {
   facets: {
     hasLoaded: true,
     resource: 'facets',
-    records: [],
+    records: [{
+      'items.effectiveLocationId': 'effectiveLocationId1',
+      'holdings.permanentLocationId': 'permanentLocationId1',
+      'holdings.statisticalCodeIds': 'statisticalCodeIds1',
+      'holdings.discoverySuppress': 'discoverySuppress1',
+      'holdings.sourceId': 'sourceId1',
+      'holdingsTags': 'holdingsTags1',
+      'holdings.holdingsTypeId': 'holdingsTypeId1',
+    }],
     other: { totalRecords: 0 }
   },
 };
@@ -27,10 +61,11 @@ const data = {
   tagsRecords: [],
   natureOfContentTerms: [],
   query: [],
-  onFetchFacets: noop,
+  onFetchFacets: jest.fn(),
   parentResources: resources,
 };
-
+const onChange = jest.fn();
+const onClear = jest.fn();
 const renderHoldingsRecordFilters = () => {
   return renderWithIntl(
     <Router>
@@ -38,33 +73,92 @@ const renderHoldingsRecordFilters = () => {
         <HoldingsRecordFilters
           activeFilters={{ 'language': ['eng'] }}
           data={data}
-          onChange={noop}
-          onClear={noop}
+          onChange={onChange}
+          onClear={onClear}
           parentResources={resources}
         />
       </ModuleHierarchyProvider>
-    </Router>
+    </Router>,
+    translationsProperties
   );
 };
 
 describe('HoldingsRecordFilters', () => {
   beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('Contains a filter for creation date ', () => {
     renderHoldingsRecordFilters();
+    expect(document.querySelector('[name="holdingsCreatedDate"]')).toBeInTheDocument();
   });
 
-  it('Contains a filter for creation date', () => {
-    expect(document.querySelector('#holdingsCreatedDate')).toBeInTheDocument();
+  it('Contains a filter for update date ', () => {
+    renderHoldingsRecordFilters();
+    expect(document.querySelector('[name="holdingsUpdatedDate"]')).toBeInTheDocument();
   });
 
-  it('Contains a filter for update date', () => {
-    expect(document.querySelector('#holdingsUpdatedDate')).toBeInTheDocument();
+  it('Should Triger effectiveLocation button', () => {
+    renderHoldingsRecordFilters();
+    const effectiveLocation = screen.getByRole('button', { name: 'effectiveLocation' });
+    userEvent.click(effectiveLocation);
+    screen.debug(undefined, Infinity);
+    expect(onClear).toHaveBeenCalledTimes(1);
+    expect(effectiveLocation).toBeEnabled();
   });
 
-  it('Contains a filter for statistical code', () => {
-    expect(document.querySelector('#holdingsStatisticalCodeIds')).toBeInTheDocument();
+  it('Should Triger holdingsPermanentLocation button', () => {
+    renderHoldingsRecordFilters();
+    const holdingsPermanentLocation = screen.getByRole('button', { name: 'holdingsPermanentLocation' });
+    userEvent.click(holdingsPermanentLocation);
+    expect(onClear).toHaveBeenCalledTimes(1);
+    expect(holdingsPermanentLocation).toBeEnabled();
   });
 
-  it('Contains a filter for source', () => {
-    expect(document.querySelector('#holdingsSource')).toBeInTheDocument();
+  it('Should Triger holdingsType button', () => {
+    renderHoldingsRecordFilters();
+    const holdingsType = screen.getByRole('button', { name: 'holdingsType' });
+    userEvent.click(holdingsType);
+    expect(onClear).toHaveBeenCalledTimes(1);
+    expect(holdingsType).toBeEnabled();
+  });
+
+  it('Should Triger holdingsDiscoverySuppress button', () => {
+    renderHoldingsRecordFilters();
+    const holdingsDiscoverySuppress = screen.getByRole('button', { name: 'holdingsDiscoverySuppress' });
+    userEvent.click(holdingsDiscoverySuppress);
+    expect(onClear).toHaveBeenCalledTimes(1);
+    expect(holdingsDiscoverySuppress).toBeEnabled();
+  });
+
+  it('Should Triger holdingsStatisticalCodeIds button', () => {
+    renderHoldingsRecordFilters();
+    const holdingsStatisticalCodeIds = screen.getByRole('button', { name: 'holdingsStatisticalCodeIds' });
+    userEvent.click(holdingsStatisticalCodeIds);
+    expect(onClear).toHaveBeenCalledTimes(1);
+    expect(holdingsStatisticalCodeIds).toBeEnabled();
+  });
+
+  it('Should Triger holdingsCreatedDate button', () => {
+    renderHoldingsRecordFilters();
+    const holdingsCreatedDate = screen.getByRole('button', { name: 'holdingsCreatedDate' });
+    userEvent.click(holdingsCreatedDate);
+    expect(onClear).toHaveBeenCalledTimes(1);
+    expect(holdingsCreatedDate).toBeEnabled();
+  });
+
+  it('Should Triger holdingsUpdatedDate button', () => {
+    renderHoldingsRecordFilters();
+    const holdingsUpdatedDate = screen.getByRole('button', { name: 'holdingsUpdatedDate' });
+    userEvent.click(holdingsUpdatedDate);
+    expect(onClear).toHaveBeenCalledTimes(1);
+    expect(holdingsUpdatedDate).toBeEnabled();
+  });
+
+  it('Should Triger holdingsSource button', () => {
+    renderHoldingsRecordFilters();
+    const holdingsSource = screen.getByRole('button', { name: 'holdingsSource' });
+    userEvent.click(holdingsSource);
+    expect(onClear).toHaveBeenCalledTimes(1);
+    expect(holdingsSource).toBeEnabled();
   });
 });
