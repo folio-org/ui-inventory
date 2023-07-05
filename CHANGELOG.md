43ace06459425de80143a07a32f58a56596a04f7
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2023-07-05 17:31:56 +0300

    UIIN-2385: Restrict modifying and deleting system call number types.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -41,6 +41,7 @@
 * Statistical Code dropdown "contains" type ahead functionality needed (Instance/Holdings/Items). Refs UIIN-2466.
 * Fix problem with a large number of items on a single holdings record. Refs UIIN-2478.
 * Decrease rerenders for TargetProfileDetail component to avoid errors when view the target profile. Fixes UIIN-2467.
+* Restrict modifying and deleting system call number types. Refs UIIN-2385.
 
 ## [9.4.8](https://github.com/folio-org/ui-inventory/tree/v9.4.8) (2023-06-30)
 [Full Changelog](https://github.com/folio-org/ui-inventory/compare/v9.4.7...v9.4.8)
--- src/settings/CallNumberTypes.test.js
+++ src/settings/CallNumberTypes.test.js
@@ -3,6 +3,10 @@ import { MemoryRouter } from 'react-router-dom';
 
 import '../../test/jest/__mock__';
 
+import { ControlledVocab } from '@folio/stripes/smart-components';
+
+import * as utils from '../utils';
+
 import {
   renderWithIntl,
   stripesStub,
@@ -10,6 +14,8 @@ import {
 } from '../../test/jest/helpers';
 
 import CallNumberTypes from './CallNumberTypes';
+
+jest.mock('../utils');
 
 const defaultProps = {
   stripes: {
@@ -33,5 +39,18 @@ describe('CallNumberTypes', () => {
   it('should render properly', () => {
     const { getByText } = renderCallNumberTypes();
     expect(getByText('ControlledVocab')).toBeInTheDocument();
+  });
+
+  describe('when call number type is "system"', () => {
+    it('should hide the "edit" and "delete" buttons', () => {
+      renderCallNumberTypes();
+
+      const actionSuppressor = {
+        delete: expect(utils.sourceSuppressor).toHaveBeenCalledWith('system'),
+        edit: expect(utils.sourceSuppressor).toHaveBeenCalledWith('system'),
+      };
+
+      expect(ControlledVocab).toHaveBeenCalledWith(expect.objectContaining({ actionSuppressor }), {});
+    });
   });
 });
