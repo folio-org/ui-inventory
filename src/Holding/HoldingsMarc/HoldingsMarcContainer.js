65a83b3f2ab23a95b8eb68215cf62052f8ed5518
Mariia_Aloshyna <Mariia_Aloshyna@epam.com>
2023-08-30 13:20:10 +0300

    UIIN-2449: refactor containers

--- src/Holding/HoldingsMarc/HoldingsMarcContainer.js
+++ src/Holding/HoldingsMarc/HoldingsMarcContainer.js
@@ -11,7 +11,7 @@ const HoldingsMarcContainer = ({
   instanceId,
   holdingsrecordid,
 }) => {
-  const { instance = {}, isLoading: isInstanceLoading } = useInstance(instanceId);
+  const { instance, isLoading: isInstanceLoading } = useInstance(instanceId);
 
   return (
     <ViewSource
