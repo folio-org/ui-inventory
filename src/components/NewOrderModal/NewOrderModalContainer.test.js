664357f75442b58661c962fcd7ed49a97f645026
Yury Saukou <yury_saukou@epam.com>
2023-10-12 13:23:49 +0400

    UIIN-2614 Provide an instance `tenantId` to the PO line form when creating an order from the instance (#2299)
    * UIIN-2614 Provide an instance tenantId to the PO line form when creating an order from the instance
    
    * update tests

--- src/components/NewOrderModal/NewOrderModalContainer.test.js
+++ src/components/NewOrderModal/NewOrderModalContainer.test.js
@@ -1,23 +1,30 @@
-import React from 'react';
 import {
   BrowserRouter as Router,
   useHistory,
 } from 'react-router-dom';
-import { act, render, screen, fireEvent } from '@folio/jest-config-stripes/testing-library/react';
+
+import userEvent from '@folio/jest-config-stripes/testing-library/user-event';
+import { render, screen } from '@folio/jest-config-stripes/testing-library/react';
+import { useOkapiKy } from '@folio/stripes/core';
 
 import '../../../test/jest/__mock__';
 
-// eslint-disable-next-line import/order
-import { useOkapiKy } from '@folio/stripes/core';
-
 import { translationsProperties } from '../../../test/jest/helpers';
 import Harness from '../../../test/jest/helpers/Harness';
+import { instance } from '../../../test/fixtures/instance';
+import { useInstance } from '../../common';
 import NewOrderModalContainer from './NewOrderModalContainer';
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useHistory: jest.fn()
 }));
+jest.mock('../../common', () => ({
+  ...jest.requireActual('../../common'),
+  useInstance: jest.fn(),
+}));
+
+global.document.createRange = jest.fn(() => new Range());
 
 const defaultProps = {
   onCancel: jest.fn(),
@@ -44,18 +51,25 @@ describe('NewOrderModalContainer', () => {
   const historyMock = {
     push: jest.fn(),
   };
+  const order = {
+    id: 'orderId',
+  };
   const kyMock = {
     get: jest.fn(() => ({
       json: () => Promise.resolve({
-        purchaseOrders: [{ id: 'orderId' }],
+        purchaseOrders: [order],
       })
     }))
   };
 
   beforeEach(() => {
+    historyMock.push.mockClear();
     useHistory
       .mockClear()
       .mockReturnValue(historyMock);
+    useInstance
+      .mockClear()
+      .mockReturnValue({ instance });
     useOkapiKy
       .mockClear()
       .mockReturnValue(kyMock);
@@ -70,7 +84,7 @@ describe('NewOrderModalContainer', () => {
   it('should navigate to \'PO\' creation form when create btn was clicked and \'PO number\' field is empty', async () => {
     renderNewOrderModalContainer();
 
-    await act(async () => fireEvent.click(screen.getByText(/^Create$/)));
+    await userEvent.click(screen.getByText(/^Create$/));
 
     expect(historyMock.push).toHaveBeenCalledWith('/orders/create', expect.anything());
   });
@@ -78,9 +92,19 @@ describe('NewOrderModalContainer', () => {
   it('should navigate to \'PO Line\' creation form when create btn was clicked and PO is exist', async () => {
     renderNewOrderModalContainer();
 
-    await act(async () => fireEvent.change(screen.getByLabelText(/PO number/i), { target: { value: '123' } }));
-    await act(async () => fireEvent.click(screen.getByText(/^Create$/)));
+    await userEvent.type(screen.getByLabelText(/PO number/i), '123');
+    await userEvent.tab();
 
-    expect(historyMock.push).toHaveBeenCalledWith('/orders/create', { 'instanceId': undefined });
+    expect(screen.queryByText('ui-inventory.newOrder.modal.PONumber.doesNotExist')).not.toBeInTheDocument();
+
+    await userEvent.click(await screen.findByText(/^Create$/));
+
+    expect(historyMock.push).toHaveBeenCalledWith(
+      `/orders/view/${order.id}/po-line/create`,
+      {
+        instanceId: instance.id,
+        instanceTenantId: 'diku',
+      },
+    );
   });
 });
