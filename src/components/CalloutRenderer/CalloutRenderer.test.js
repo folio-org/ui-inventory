2a7b9ff516339589ca6d7b597bbc74468bc13abb
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2024-04-23 10:31:20 +0400

    UIIN-2665: Jest/RTL: Cover CalloutRenderer component with unit tests

--- src/components/CalloutRenderer/CalloutRenderer.test.js
+++ src/components/CalloutRenderer/CalloutRenderer.test.js
@@ -3,9 +3,8 @@ import {
   render
 } from '@folio/jest-config-stripes/testing-library/react';
 import CalloutRenderer from './CalloutRenderer';
-import useCallout from '../../hooks/useCallout'; // Import the useCallout hook directly
+import useCallout from '../../hooks/useCallout';
 
-// Mock useCallout hook
 jest.mock('../../hooks/useCallout');
 
 const MESSAGES = {
@@ -27,13 +26,10 @@ describe('CalloutRenderer', () => {
   it('should render and call sendCallout with correct arguments', () => {
     const message = 'This is a test message';
 
-    // Render the component
     render(<CalloutRenderer message={message} type={MESSAGES.ERROR} />);
 
-    // Assert that useCallout hook was called
     expect(useCallout).toHaveBeenCalled();
 
-    // Assert that sendCallout was called with the correct arguments in useEffect
     expect(useCallout().sendCallout).toHaveBeenCalledWith({
       type: MESSAGES.ERROR,
       message: 'This is a test message'
@@ -43,13 +39,10 @@ describe('CalloutRenderer', () => {
   it('should render and call sendCallout with default type', () => {
     const message = 'This is a test message';
 
-    // Render the component
     render(<CalloutRenderer message={message} />);
 
-    // Assert that useCallout hook was called
     expect(useCallout).toHaveBeenCalled();
 
-    // Assert that sendCallout was called with the correct arguments in useEffect
     expect(useCallout().sendCallout).toHaveBeenCalledWith({
       type: MESSAGES.SUCCESS,
       message: 'This is a test message'
