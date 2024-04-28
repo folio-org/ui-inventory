83eb2eb1d69b493cc178b54a70d44e0584155649
vashjs <vadym.shchekotilin@gmail.com>
2024-04-28 20:36:43 +0200

    UIIN-2664 Jest/RTL: Cover MoveHoldingContext component with unit tests

---
+++ test/fixtures/holdingsById.js
@@ -0,0 +1,136 @@
+export const holdingsById = {
+  '0c45bb50-7c9b-48b0-86eb-178a494e25fe': {
+    'holdingsItems': [],
+    'bareHoldingsItems': [],
+    'id': '0c45bb50-7c9b-48b0-86eb-178a494e25fe',
+    '_version': 1,
+    'hrid': 'hold000000000002',
+    'formerIds': [
+      'ABW4508',
+      '442882'
+    ],
+    'instanceId': '69640328-788e-43fc-9c3c-af39e243f3b7',
+    'permanentLocationId': 'fcd64ce1-6995-48f0-840e-89ffa2288371',
+    'effectiveLocationId': 'fcd64ce1-6995-48f0-840e-89ffa2288371',
+    'electronicAccess': [
+      {
+        'uri': 'http://www.ebscohost.com',
+        'materialsSpecification': '1984-',
+        'relationshipId': '3b430592-2e09-4b48-9a0c-0636d66b9fb3'
+      },
+      {
+        'uri': 'http://www.jstor.com',
+        'materialsSpecification': '1984-',
+        'publicNote': 'Most recent 4 years not available.',
+        'relationshipId': '3b430592-2e09-4b48-9a0c-0636d66b9fb3'
+      }
+    ],
+    'callNumber': 'K1 .M44',
+    'acquisitionMethod': 'Purchase',
+    'receiptStatus': 'Not currently received',
+    'administrativeNotes': [],
+    'notes': [
+      {
+        'holdingsNoteTypeId': 'b160f13a-ddba-4053-b9c4-60ec5ea45d56',
+        'note': ' Subscription cancelled per Evans Current Periodicals Selector Review. acq',
+        'staffOnly': true
+      },
+      {
+        'holdingsNoteTypeId': 'b160f13a-ddba-4053-b9c4-60ec5ea45d56',
+        'note': 'Asked Ebsco to check with publisher and ask what years were paid since we are missing (2001:Oct.-Dec.), (All of 2002), & (2003:Jan.-Feb.). 20030305. evaldez',
+        'staffOnly': false
+      },
+      {
+        'holdingsNoteTypeId': 'b160f13a-ddba-4053-b9c4-60ec5ea45d56',
+        'note': 'Backorder:v.87(2001:Oct.-Dec)-v.88(2002). eluza',
+        'staffOnly': false
+      },
+      {
+        'holdingsNoteTypeId': 'b160f13a-ddba-4053-b9c4-60ec5ea45d56',
+        'note': 'WITH 2010 TREAT ISSUE S AS DISCARDS. dgill',
+        'staffOnly': false
+      }
+    ],
+    'retentionPolicy': 'Permanently retained.',
+    'holdingsStatements': [
+      {
+        'statement': 'v.70-84 (1984-1998)'
+      },
+      {
+        'statement': 'v.85:no. 1-11 (1999:Jan.-Nov.)'
+      },
+      {
+        'statement': 'v.87:no.1-9 (2001:Jan.-Sept.)'
+      },
+      {
+        'statement': 'v.89:no.2-12 (2003:Feb.-Dec.)'
+      },
+      {
+        'statement': 'v.90-95 (2004-2009)'
+      }
+    ],
+    'holdingsStatementsForIndexes': [],
+    'holdingsStatementsForSupplements': [
+      {
+        'statement': 'no.1-23 '
+      }
+    ],
+    'copyNumber': '1',
+    'statisticalCodeIds': [
+      '775b6ad4-9c35-4d29-bf78-8775a9b42226'
+    ],
+    'metadata': {
+      'createdDate': '2024-04-27T13:50:16.043+00:00',
+      'updatedDate': '2024-04-27T13:50:16.043+00:00'
+    }
+  },
+  'c4a15834-0184-4a6f-9c0c-0ca5bad8286d': {
+    'holdingsItems': [],
+    'bareHoldingsItems': [],
+    'id': 'c4a15834-0184-4a6f-9c0c-0ca5bad8286d',
+    '_version': 3,
+    'hrid': 'hold000000000001',
+    'formerIds': [],
+    'instanceId': '69640328-788e-43fc-9c3c-af39e243f3b7',
+    'permanentLocationId': '53cf956f-c1df-410b-8bea-27f712cca7c0',
+    'effectiveLocationId': '53cf956f-c1df-410b-8bea-27f712cca7c0',
+    'electronicAccess': [
+      {
+        'uri': 'https://search.proquest.com/publication/1396348',
+        'materialsSpecification': '1.2012 -',
+        'publicNote': 'via ProQuest, the last 12 months are not available due to an embargo',
+        'relationshipId': 'f5d0068e-6272-458e-8a81-b85e7b9a14aa'
+      },
+      {
+        'uri': 'https://www.emeraldinsight.com/loi/jepp',
+        'materialsSpecification': '1.2012 -',
+        'publicNote': 'via Emerald',
+        'relationshipId': 'f5d0068e-6272-458e-8a81-b85e7b9a14aa'
+      },
+      {
+        'uri': 'https://www.emeraldinsight.com/journal/jepp',
+        'materialsSpecification': '1.2012 - 5.2016',
+        'publicNote': 'via Emerald, national license',
+        'relationshipId': 'f5d0068e-6272-458e-8a81-b85e7b9a14aa'
+      }
+    ],
+    'callNumber': 'K1 .M44',
+    'administrativeNotes': [
+      'cataloging note'
+    ],
+    'notes': [],
+    'holdingsStatements': [
+      {
+        'statement': '1.2012 -'
+      }
+    ],
+    'holdingsStatementsForIndexes': [],
+    'holdingsStatementsForSupplements': [],
+    'statisticalCodeIds': [],
+    'metadata': {
+      'createdDate': '2024-04-27T13:50:16.060+00:00',
+      'updatedDate': '2024-04-28T12:12:07.062+00:00',
+      'updatedByUserId': '7e70e5c1-20d1-56aa-8cd7-1d92d9fa83f3'
+    }
+  }
+};
