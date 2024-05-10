8fbeeaa0f94b8ee04836d1bc84f8e5207c2c2cd1
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2024-05-10 13:48:22 +0400

    UIIN-2669: fix comments

--- src/components/ModalContent/ModalContent.test.js
+++ src/components/ModalContent/ModalContent.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { fireEvent } from '@folio/jest-config-stripes/testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+
+import { fireEvent } from '@folio/jest-config-stripes/testing-library/react';
+
 import ModalContent from './ModalContent';
 import { renderWithIntl } from '../../../test/jest/helpers';
 import '../../../test/jest/__mock__';
@@ -17,36 +19,47 @@ const itemMock = {
   },
 };
 
-describe('ModalContent', () => {
-  it('renders with correct props and messages', () => {
-    const onCancelMock = jest.fn();
-    const onConfirmMock = jest.fn();
-    const requestsUrl = '/requests';
-    const itemRequestCount = 2;
-    const status = 'Available';
 
-    const { getByText, getByRole } = renderWithIntl(
-      <MemoryRouter>
-        <ModalContent
-          stripes={stripesMock}
-          item={itemMock}
-          status={status}
-          requestsUrl={requestsUrl}
-          onCancel={onCancelMock}
-          onConfirm={onConfirmMock}
-          itemRequestCount={itemRequestCount}
-        />
-      </MemoryRouter>
-    );
+describe('ModalContent', () => {
+  const onCancelMock = jest.fn();
+  const onConfirmMock = jest.fn();
+  const requestsUrl = '/requests';
+  const itemRequestCount = 2;
+  const status = 'Available';
+
+  const renderModalContent = () => renderWithIntl(
+    <MemoryRouter>
+      <ModalContent
+        stripes={stripesMock}
+        item={itemMock}
+        status={status}
+        requestsUrl={requestsUrl}
+        onCancel={onCancelMock}
+        onConfirm={onConfirmMock}
+        itemRequestCount={itemRequestCount}
+      />
+    </MemoryRouter>
+  );
+  it('renders modal content', () => {
+    const { getByText } = renderModalContent();
 
     expect(getByText(/confirmModal.message/)).toBeInTheDocument();
     expect(getByText(/confirmModal.requestMessage/)).toBeInTheDocument();
+  });
+
+  it('should render modal and call cancel on click', () => {
+    const { getByRole } = renderModalContent();
 
     const cancelButton = getByRole('button', { name: /cancel/i });
-    const confirmButton = getByRole('button', { name: /confirm/i });
 
     fireEvent.click(cancelButton);
     expect(onCancelMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('should render modal and call confirm on click', () => {
+    const { getByRole } = renderModalContent();
+
+    const confirmButton = getByRole('button', { name: /confirm/i });
 
     fireEvent.click(confirmButton);
     expect(onConfirmMock).toHaveBeenCalledTimes(1);
