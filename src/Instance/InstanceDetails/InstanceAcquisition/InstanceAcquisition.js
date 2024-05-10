89f6fc966fe98b787841c2c9c5f366b634ef3da1
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2024-05-10 15:33:07 +0300

    UIIN-2793: Populate Acquisitions accordion on instance when central ordering is active

--- src/Instance/InstanceDetails/InstanceAcquisition/InstanceAcquisition.js
+++ src/Instance/InstanceDetails/InstanceAcquisition/InstanceAcquisition.js
@@ -1,66 +1,86 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { Link } from 'react-router-dom';
 
 import { useStripes } from '@folio/stripes/core';
-import {
-  Accordion,
-  MultiColumnList,
-  NoValue,
-} from '@folio/stripes/components';
+import { Accordion } from '@folio/stripes/components';
 
-import { getDateWithTime } from '../../../utils';
 import { useControlledAccordion } from '../../../common';
+import { isUserInConsortiumMode } from '../../../utils';
 import useInstanceAcquisition from './useInstanceAcquisition';
 
-const visibleColumns = ['poLineNumber', 'orderStatus', 'polReceiptStatus', 'dateOrdered', 'acqUnit', 'orderType'];
-const columnMapping = {
-  poLineNumber: <FormattedMessage id="ui-inventory.acq.polNumber" />,
-  orderStatus: <FormattedMessage id="ui-inventory.acq.orderStatus" />,
-  polReceiptStatus: <FormattedMessage id="ui-inventory.acq.receiptStatus" />,
-  dateOrdered: <FormattedMessage id="ui-inventory.acq.dateOpened" />,
-  acqUnit: <FormattedMessage id="ui-inventory.acq.acqUnit" />,
-  orderType: <FormattedMessage id="ui-inventory.acq.orderType" />,
-};
-const formatter = {
-  poLineNumber: i => <Link to={`/orders/lines/view/${i.id}`}>{i.poLineNumber}</Link>,
-  orderStatus: i => (
-    <>
-      {i.order?.workflowStatus ? <FormattedMessage id={`ui-inventory.acq.orderStatus.${i.order.workflowStatus}`} /> : <NoValue />}
-      {i.order?.orderCloseReason?.reason && ` - ${i.order.orderCloseReason.reason}`}
-    </>
-  ),
-  polReceiptStatus: i => <FormattedMessage id={`ui-inventory.acq.receiptStatus.${i.receiptStatus}`} />,
-  dateOrdered: i => getDateWithTime(i.order?.dateOrdered),
-  acqUnit: i => i.order?.acqUnits?.map(u => u.name)?.join(', ') || <NoValue />,
-  orderType: i => (i.order?.orderType ? <FormattedMessage id={`ui-inventory.acq.orderType.${i.order.orderType}`} /> : <NoValue />),
-};
+import TenantAcquisition from './TenantAcquisition';
+
+import css from './TenantAcquisition.css';
 
 const InstanceAcquisition = ({ accordionId, instanceId }) => {
   const stripes = useStripes();
-  const { isLoading, instanceAcquisition } = useInstanceAcquisition(instanceId);
-  const controlledAccorion = useControlledAccordion(Boolean(instanceAcquisition?.length));
+  const activeTenant = stripes.okapi.tenant;
+  const centralTenant = stripes.user.user?.consortium?.centralTenantId;
+
+  const {
+    isLoading: isLoadingActiveTenantAcquisition,
+    instanceAcquisition: activeTenantAcquisition,
+  } = useInstanceAcquisition(instanceId, activeTenant);
+  const {
+    isLoading: isLoadingCentralTenantAcquisition,
+    instanceAcquisition: centralTenantAcquisition,
+  } = useInstanceAcquisition(instanceId, centralTenant);
+
+  const controlledAcqAccorion = useControlledAccordion(Boolean(activeTenantAcquisition?.length || centralTenantAcquisition?.length));
+  const controlledActiveTenantAcqAccorion = useControlledAccordion(Boolean(activeTenantAcquisition?.length));
+  const controlledCetralTenantAcqAccorion = useControlledAccordion(Boolean(centralTenantAcquisition?.length));
 
   if (!(stripes.hasInterface('order-lines') &&
     stripes.hasInterface('orders') &&
     stripes.hasInterface('acquisitions-units'))) return null;
 
+  const getTenantAccordionLabel = (tenants, id) => tenants?.find(tenant => tenant.id === id).name;
+
   return (
     <Accordion
       id={accordionId}
       label={<FormattedMessage id="ui-inventory.acquisition" />}
-      {...controlledAccorion}
+      {...controlledAcqAccorion}
     >
-      <MultiColumnList
-        id="list-instance-acquisitions"
-        loading={isLoading}
-        contentData={instanceAcquisition}
-        visibleColumns={visibleColumns}
-        columnMapping={columnMapping}
-        formatter={formatter}
-        interactive={false}
-      />
+      {isUserInConsortiumMode(stripes) ? (
+        <>
+          <Accordion
+            id="active-tenant-acquisition-accordion"
+            label={getTenantAccordionLabel(stripes.user.user.tenants, activeTenant)}
+            className={css.tenantAcquisitionAccordion}
+            {...controlledActiveTenantAcqAccorion}
+          >
+            <TenantAcquisition
+              acquisitions={activeTenantAcquisition}
+              isLoading={isLoadingActiveTenantAcquisition}
+              tenantId={activeTenant}
+            />
+          </Accordion>
+
+          {activeTenant !== centralTenant && (
+            <Accordion
+              id="central-tenant-acquisition-accordion"
+              label={getTenantAccordionLabel(stripes.user.user.tenants, centralTenant)}
+              className={css.tenantAcquisitionAccordion}
+              {...controlledCetralTenantAcqAccorion}
+            >
+              <TenantAcquisition
+                acquisitions={centralTenantAcquisition}
+                isLoading={isLoadingCentralTenantAcquisition}
+                tenantId={centralTenant}
+              />
+            </Accordion>
+          )}
+        </>
+      ) : (
+        <TenantAcquisition
+          acquisitions={activeTenantAcquisition}
+          isLoading={isLoadingActiveTenantAcquisition}
+          tenantId={activeTenant}
+        />
+      )
+      }
     </Accordion>
   );
 };
--- src/Instance/InstanceDetails/InstanceAcquisition/InstanceAcquisition.test.js
+++ src/Instance/InstanceDetails/InstanceAcquisition/InstanceAcquisition.test.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { useStripes } from '@folio/stripes/core';
 import { screen } from '@folio/jest-config-stripes/testing-library/react';
 
 import '../../../../test/jest/__mock__';
-import renderWithIntl from '../../../../test/jest/helpers/renderWithIntl';
+import {
+  renderWithIntl,
+  translationsProperties,
+} from '../../../../test/jest/helpers';
 
 import { resultData, line } from './fixtures';
 import InstanceAcquisition from './InstanceAcquisition';
 import useInstanceAcquisition from './useInstanceAcquisition';
 
+import * as utils from '../../../utils';
+
+
+jest.mock('@folio/stripes/core', () => ({
+  ...jest.requireActual('@folio/stripes/core'),
+  useStripes: jest.fn(),
+}));
 jest.mock('./useInstanceAcquisition', () => jest.fn());
+
+const spyOnIsUserInConsortiumMode = jest.spyOn(utils, 'isUserInConsortiumMode');
 
 const renderInstanceAcquisition = (props = {}) => (
   renderWithIntl(
@@ -20,7 +33,8 @@ const renderInstanceAcquisition = (props = {}) => (
         accordionId="accordionId"
         {...props}
       />
-    </Router>
+    </Router>,
+    translationsProperties
   )
 );
 
@@ -29,9 +43,71 @@ describe('InstanceAcquisition', () => {
     useInstanceAcquisition.mockClear().mockReturnValue({ instanceAcquisition: resultData });
   });
 
-  it('should display fetched instance acquisition data', () => {
-    renderInstanceAcquisition({ instanceId: 'instanceId' });
+  describe('when user is non-consortial tenant', () => {
+    it('should display acquisition accordion and fetched instance acquisition data', () => {
+      spyOnIsUserInConsortiumMode.mockReturnValue(false);
+      useStripes.mockClear().mockReturnValue({
+        hasInterface: () => true,
+        okapi: { tenant: 'diku' },
+        user: { user: {} },
+      });
 
-    expect(screen.getByText(line.poLineNumber)).toBeInTheDocument();
+      const { container } = renderInstanceAcquisition({ instanceId: 'instanceId' });
+
+      expect(container.querySelector('#accordionId')).toBeInTheDocument();
+      expect(screen.getByText(line.poLineNumber)).toBeInTheDocument();
+    });
+  });
+
+  describe('when user is in central tenant', () => {
+    it('should render central tenant subaccordion with fetched instance acquisition data', () => {
+      spyOnIsUserInConsortiumMode.mockReturnValue(true);
+      useStripes.mockClear().mockReturnValue({
+        hasInterface: () => true,
+        okapi: { tenant: 'consortium' },
+        user: { user: {
+          consortium: { centralTenantId: 'consortium' },
+          tenants: [{
+            id: 'consortium',
+            name: 'Consortium',
+          }],
+        } },
+      });
+
+      const { container } = renderInstanceAcquisition({ instanceId: 'instanceId' });
+
+      expect(container.querySelector('#accordionId')).toBeInTheDocument();
+      expect(container.querySelector('#active-tenant-acquisition-accordion')).toBeInTheDocument();
+      expect(screen.getByText(line.poLineNumber)).toBeInTheDocument();
+    });
+  });
+
+  describe('when user is in member tenant', () => {
+    it('should render central and member tenant subaccordion with fetched instance acquisition data', () => {
+      spyOnIsUserInConsortiumMode.mockReturnValue(true);
+      useStripes.mockClear().mockReturnValue({
+        hasInterface: () => true,
+        okapi: { tenant: 'college' },
+        user: { user: {
+          consortium: { centralTenantId: 'consortium' },
+          tenants: [{
+            id: 'consortium',
+            name: 'Consortium',
+          }, {
+            id: 'college',
+            name: 'College',
+          }],
+        } },
+      });
+
+      const { container } = renderInstanceAcquisition({ instanceId: 'instanceId' });
+
+      expect(container.querySelector('#accordionId')).toBeInTheDocument();
+      expect(container.querySelector('#active-tenant-acquisition-accordion')).toBeInTheDocument();
+      expect(screen.getAllByText(line.poLineNumber)[0]).toBeInTheDocument();
+
+      expect(container.querySelector('#central-tenant-acquisition-accordion')).toBeInTheDocument();
+      expect(screen.getAllByText(line.poLineNumber)[1]).toBeInTheDocument();
+    });
   });
 });
