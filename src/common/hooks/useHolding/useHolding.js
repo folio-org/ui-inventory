190fea6630a422254f2baaf16be84de9cb718cfa
Denys Bohdan <denys_bohdan@epam.com>
2024-06-05 13:01:43 +0200

    UIIN-2404 Edit Inventory Holdings: Display a Save & keep editing button

--- src/common/hooks/useHolding/useHolding.js
+++ src/common/hooks/useHolding/useHolding.js
@@ -8,7 +8,7 @@ const useHolding = (holdingId, { tenantId = '' } = {}) => {
   const ky = useTenantKy({ tenantId });
   const [namespace] = useNamespace({ key: 'holding' });
 
-  const { isLoading, data: holding = {} } = useQuery(
+  const { isLoading, data: holding = {}, refetch } = useQuery(
     [namespace, holdingId],
     () => ky.get(`holdings-storage/holdings/${holdingId}`).json(),
     { enabled: Boolean(holdingId) },
@@ -17,6 +17,7 @@ const useHolding = (holdingId, { tenantId = '' } = {}) => {
   return ({
     isLoading,
     holding,
+    refetch,
   });
 };
 
