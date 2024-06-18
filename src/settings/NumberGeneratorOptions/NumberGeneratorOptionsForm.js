b0a16b667e11780bd9dca40b4cdbc95338d493fb
Ethan Freestone <ethan.freestone@k-int.co.uk>
2024-06-18 09:44:44 +0100

    chore: Removed "Learn more" button

--- src/settings/NumberGeneratorOptions/NumberGeneratorOptionsForm.js
+++ src/settings/NumberGeneratorOptions/NumberGeneratorOptionsForm.js
@@ -234,16 +234,6 @@ const NumberGeneratorOptionsForm = () => {
                                   }}
                                 />
                               </Layout>
-                              <Layout className="display-flex flex-direction-column flex-align-items-center">
-                                <Button
-                                  allowAnchorClick
-                                  buttonStyle="primary"
-                                  href="https://wiki.folio.org/display/FOLIOtips/Number+generator"
-                                  marginBottom0
-                                >
-                                  <FormattedMessage id="ui-inventory.settings.learnMore" />
-                                </Button>
-                              </Layout>
                             </Layout>
                           }
                           iconSize="medium"
