84fc363800bcbcead6eaa646898ce6d4db934b27
Mariia_Aloshyna <Mariia_Aloshyna@epam.com>
2024-07-08 23:39:10 +0300

    UIIN-2943: Trigger search from icon in related titles on Instance detail view

--- src/components/TitlesView/TitlesView.js
+++ src/components/TitlesView/TitlesView.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
@@ -6,9 +5,19 @@ import { Link } from 'react-router-dom';
 import {
   MultiColumnList,
   KeyValue,
+  IconButton,
+  Tooltip,
 } from '@folio/stripes/components';
+import {
+  queryIndexes,
+  ADVANCED_SEARCH_INDEX,
+  advancedSearchQueryBuilder,
+} from '@folio/stripes-inventory-components';
 
-import { getIdentifiers } from '../../utils';
+import {
+  getIdentifiers,
+  getIdentifiersValues,
+} from '../../utils';
 import { indentifierTypeNames } from '../../constants';
 
 const TitlesViews = ({
@@ -22,14 +31,71 @@ const TitlesViews = ({
     ISSN,
     ISBN,
   } = indentifierTypeNames;
+  const createInstanceLink = (row) => {
+    const identifiersValues = getIdentifiersValues(row.identifiers, [ISBN, ISSN], identifierTypesById);
+    const isbn = identifiersValues?.[ISBN];
+    const issn = identifiersValues?.[ISSN];
+
+    const advancedSearchRows = [
+      {
+        bool: '',
+        match: 'exactPhrase',
+        searchOption: queryIndexes.TITLE,
+        query: row.title,
+      },
+      ...(isbn ? [{
+        bool: 'and',
+        match: 'containsAll',
+        searchOption: queryIndexes.ISBN,
+        query: isbn,
+      }] : []),
+      ...(issn ? [{
+        bool: 'and',
+        match: 'containsAll',
+        searchOption: queryIndexes.ISSN,
+        query: issn,
+      }] : [])
+    ];
+
+    const builtQuery = advancedSearchQueryBuilder(advancedSearchRows);
+
+    const searchParams = new URLSearchParams({
+      qindex: ADVANCED_SEARCH_INDEX,
+      query: builtQuery,
+    });
+
+    return `/inventory?${searchParams.toString()}`;
+  };
+
   const formatter = {
-    title: row => (row[titleKey] ?
-      <Link
-        to={`/inventory/view/${row[titleKey]}`}
-      >
-        {row.title}
-      </Link> :
-      row.title || '-'),
+    title: row => (row[titleKey]
+      ?
+        <Link
+          to={`/inventory/view/${row[titleKey]}`}
+        >
+          {row.title}
+        </Link>
+      : row.title
+        ?
+          <>
+            {row.title}
+            <Tooltip
+              id="searchForTitle"
+              text={<FormattedMessage id="ui-inventory.searchForTitle" values={{ title: row.title }} />}
+            >
+              {({ ref, ariaIds }) => (
+                <IconButton
+                  ref={ref}
+                  icon="search"
+                  to={createInstanceLink(row)}
+                  target="_blank"
+                  aria-labelledby={ariaIds.text}
+                />
+              )}
+            </Tooltip>
+          </>
+        : '-'
+    ),
     hrid: row => row.hrid || '-',
     issn: row => getIdentifiers(row.identifiers, ISSN, identifierTypesById) || '-',
     isbn: row => getIdentifiers(row.identifiers, ISBN, identifierTypesById) || '-',
--- src/utils.js
+++ src/utils.js
@@ -405,6 +405,31 @@ export const getIdentifiers = (identifiers = [], type, identifierTypesById) => {
     </div>);
 };
 
+/**
+ * Creates an object with identifier type as a key and its value
+ *
+ * @param {Object[]} identifiers
+ * @param {String[]} types identifier names
+ * @param {Object} identifierTypesById
+ *
+ * @returns {Object} Identifier names and its values
+ */
+export const getIdentifiersValues = (identifiers = [], types, identifierTypesById) => {
+  const result = {};
+
+  if (!identifiers.length) return result;
+
+  identifiers.forEach(({ identifierTypeId, value }) => {
+    const ident = identifierTypesById[identifierTypeId];
+
+    if (types.some(type => type === ident?.name) && value) {
+      result[ident.name] = value;
+    }
+  });
+
+  return result;
+};
+
 export const updateOrAddIdentifier = (identifiers, identifierTypeId, value) => {
   const index = identifiers.findIndex(identifier => identifier.identifierTypeId === identifierTypeId);
   const indentifier = { identifierTypeId, value };
