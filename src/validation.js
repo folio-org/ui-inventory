3a05c17fb5db0a1b9a1e0d6c5fe4fe3603602f7c
Denys Bohdan <denys_bohdan@epam.com>
2024-09-09 14:09:23 +0200

    UIIN-2849 Instance Detail/Edit/New: Add new section for Date type, Date 1, & Date 2 (#2583)
    * UIIN-2889 Trim call numbers when Creating/Editing/Deriving Holdings/Items
    
    * UIIN-2849 Adding Date 1 and Date 2 to Instance form
    
    * UIIN-2849 use Data export: Can upload files, export, download files and view logs to check for Export p
    
    * UIIN-2849 Instance Detail/Edit/New: Add new section for Date type, Date 1, & Date 2
    
    * UIIN-2849 added tests for new Date fields
    
    * UIIN-2849 Move useInstanceDateTypes to stripes-inventory-components
    
    ---------
    
    Co-authored-by: Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>

--- src/validation.js
+++ src/validation.js
@@ -1,3 +1,5 @@
+export const DATE_LENGTH = 4;
+
 export const validateTitles = (instance, type, errors, message) => {
   const titleAttr = `${type}Titles`;
   const errorList = [];
@@ -27,6 +29,22 @@ export const validateSubInstances = (instance, type, errors, message) => {
 
   if (errorList.length) {
     errors[type] = errorList;
+  }
+};
+
+export const validateDates = (instance, errors, dateLengthMessage) => {
+  errors.dates = {};
+
+  const date1Length = instance.dates?.date1?.length;
+  const date2Length = instance.dates?.date2?.length;
+
+  // dates are not required, so the can be empty, but if they're not empty - they must be 4 characters long
+  if (date1Length && date1Length !== DATE_LENGTH) {
+    errors.dates.date1 = dateLengthMessage;
+  }
+
+  if (date2Length && date2Length !== DATE_LENGTH) {
+    errors.dates.date2 = dateLengthMessage;
   }
 };
 
