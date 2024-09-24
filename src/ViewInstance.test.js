95399b6ec4a40865039f96c2fd7b8c05858bce20
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2024-09-24 13:55:44 +0500

    test: update test case

--- src/ViewInstance.test.js
+++ src/ViewInstance.test.js
@@ -737,6 +737,7 @@ describe('ViewInstance', () => {
         renderViewInstance();
         fireEvent.click(screen.getByRole('button', { name: 'Actions' }));
         fireEvent.click(screen.getByRole('button', { name: 'New order' }));
+        expect(await screen.findByText(/Create order/i)).toBeInTheDocument();
         fireEvent.click(screen.getByRole('button', { name: 'Cancel' }));
 
         await waitFor(() => {
