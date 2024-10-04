2a89814116c7590f3b674d56011f964889190cff
Yury Saukou <yury_saukou@epam.com>
2024-10-04 15:49:20 +0400

    UIIN-3054 Fetch bound item related pieces from the central tenant in the central ordering mode

--- src/common/hooks/usePiecesMutation/usePiecesMutation.js
+++ src/common/hooks/usePiecesMutation/usePiecesMutation.js
@@ -3,11 +3,17 @@ import { useMutation } from 'react-query';
 import { ORDER_PIECES_API } from '@folio/stripes-acq-components';
 import { useOkapiKy } from '@folio/stripes/core';
 
+import { extendKyWithTenant } from '../../../Instance/utils';
+
 const usePiecesMutation = (options = {}) => {
   const ky = useOkapiKy();
 
   const { mutateAsync, isLoading } = useMutation({
-    mutationFn: (piece) => ky.put(`${ORDER_PIECES_API}/${piece.id}`, { json: piece }),
+    mutationFn: (piece) => {
+      const httpClient = piece?.tenantId ? extendKyWithTenant(ky, piece.tenantId) : ky;
+
+      return httpClient.put(`${ORDER_PIECES_API}/${piece.id}`, { json: piece });
+    },
     ...options,
   });
 
