5639e3f62ce7106417f257df74d74bf3caa74a87
Yury Saukou <yury_saukou@epam.com>
2024-10-08 22:04:03 +0400

    add UIIN-3054

--- src/common/hooks/useBoundPieces/useBoundPieces.js
+++ src/common/hooks/useBoundPieces/useBoundPieces.js
@@ -1,25 +1,43 @@
 import { useQuery } from 'react-query';
 
 import {
+  getConsortiumCentralTenantId,
   LIMIT_MAX,
   ORDER_PIECES_API,
+  useCentralOrderingSettings,
 } from '@folio/stripes-acq-components';
 import {
   useNamespace,
   useOkapiKy,
+  useStripes,
 } from '@folio/stripes/core';
 
+import { extendKyWithTenant } from '../../../Instance/utils';
+import { isUserInConsortiumMode } from '../../../utils';
+
 const DEFAULT_DATA = [];
+const SORTING_FIELD = 'receivedDate';
 
 const useBoundPieces = (itemId, options = {}) => {
   const { enabled = true, ...otherOptions } = options;
+
+  const stripes = useStripes();
   const ky = useOkapiKy();
   const [namespace] = useNamespace({ key: 'bound-pieces' });
   const filterQuery = `bindItemId==${itemId} and isBound==true`;
 
+  const consortiumCentralTenantId = getConsortiumCentralTenantId(stripes);
+
+  const {
+    enabled: isCentralOrderingEnabled,
+    isFetching: isCentralOrderingSettingsFetching,
+  } = useCentralOrderingSettings({
+    enabled: stripes.hasInterface('orders-storage.settings'),
+  });
+
   const searchParams = {
     limit: LIMIT_MAX,
-    query: `${filterQuery} sortby receivedDate`,
+    query: `${filterQuery} sortby ${SORTING_FIELD}`,
   };
 
   const {
@@ -29,8 +47,34 @@ const useBoundPieces = (itemId, options = {}) => {
     refetch,
   } = useQuery({
     queryKey: [namespace, itemId],
-    queryFn: () => ky.get(ORDER_PIECES_API, { searchParams }).json(),
-    enabled: Boolean(enabled && itemId),
+    queryFn: async ({ signal }) => {
+      const makeRequest = (httpClient) => httpClient.get(ORDER_PIECES_API, { searchParams, signal }).json();
+
+      if (isCentralOrderingEnabled) {
+        const fulfilledResults = await Promise.allSettled([
+          makeRequest(ky),
+          makeRequest(extendKyWithTenant(ky, consortiumCentralTenantId))
+        ]).then((results) => results.filter(({ status }) => status === 'fulfilled'));
+
+        const totalRecords = fulfilledResults.reduce((acc, { value }) => acc + value.totalRecords, 0);
+        const pieces = fulfilledResults
+          .flatMap(({ value }, index) => value.pieces.map(piece => ({ ...piece, tenantId: index === 1 ? consortiumCentralTenantId : stripes.okapi.tenant })))
+          .sort((a, b) => new Date(a[SORTING_FIELD]) - new Date(b[SORTING_FIELD]));
+
+        return {
+          pieces,
+          totalRecords,
+        };
+      }
+
+      return makeRequest(ky);
+    },
+    enabled: Boolean(
+      enabled
+      && itemId
+      && !isCentralOrderingSettingsFetching
+      && (isUserInConsortiumMode(stripes) ? consortiumCentralTenantId : true)
+    ),
     ...otherOptions,
   });
 
--- src/common/hooks/usePiecesMutation/usePiecesMutation.js
+++ src/common/hooks/usePiecesMutation/usePiecesMutation.js
@@ -3,11 +3,17 @@ import { useMutation } from 'react-query';
 import { ORDER_PIECES_API } from '@folio/stripes-acq-components';
 import { useOkapiKy } from '@folio/stripes/core';
 
+import { extendKyWithTenant } from '../../../Instance/utils';
+
 const usePiecesMutation = (options = {}) => {
   const ky = useOkapiKy();
 
   const { mutateAsync, isLoading } = useMutation({
-    mutationFn: (piece) => ky.put(`${ORDER_PIECES_API}/${piece.id}`, { json: piece }),
+    mutationFn: ({ tenantId, ...piece }) => {
+      const httpClient = tenantId ? extendKyWithTenant(ky, tenantId) : ky;
+
+      return httpClient.put(`${ORDER_PIECES_API}/${piece.id}`, { json: piece });
+    },
     ...options,
   });
 
