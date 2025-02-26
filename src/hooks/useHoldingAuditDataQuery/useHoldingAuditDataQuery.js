01b296ecc207bc1da38a2f4454145c01100ae983
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2025-02-26 13:43:43 +0100

    UIIN-3174: Remove limit for version history list

--- src/hooks/useHoldingAuditDataQuery/useHoldingAuditDataQuery.js
+++ src/hooks/useHoldingAuditDataQuery/useHoldingAuditDataQuery.js
@@ -5,15 +5,13 @@ import {
   useOkapiKy,
 } from '@folio/stripes/core';
 
-import { LIMIT_MAX } from '@folio/stripes-inventory-components';
-
 const useHoldingAuditDataQuery = (holdingId) => {
   const ky = useOkapiKy();
   const [namespace] = useNamespace({ key: 'holding-audit-data' });
 
   const { isLoading, data = {} } = useQuery({
     queryKey: [namespace, holdingId],
-    queryFn: () => ky.get(`audit-data/inventory/holdings/${holdingId}`, { searchParams: { limit: LIMIT_MAX } }).json(),
+    queryFn: () => ky.get(`audit-data/inventory/holdings/${holdingId}`).json(),
     enabled: Boolean(holdingId),
   });
 
