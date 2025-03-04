b1ac798cadc91bd34069ea1e1cecfe2a73937de9
elsenhans <elsenhans@users.noreply.github.com>
2025-03-04 10:58:32 +0100

    UIIN-2546-use-numberGenerator-for-barcode-callNumber-accessionNumber fix CHANGELOG, fix jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const config = require('@folio/jest-config-stripes');
 
+const additionalModules = [
+  '@k-int/stripes-kint-components',
+].join('|');
+const combinedModules = config.transformIgnorePatterns[0].replace(')', `|${additionalModules})`);
+
 module.exports = {
   ...config,
   setupFiles: [
     ...config.setupFiles,
     path.join(__dirname, './test/jest/jest.setup.js'),
   ],
-  transformIgnorePatterns: [
-    '/node_modules/(?!@k-int/stripes-kint-components|@folio/.*|ky/.*|uuid/.*)',
-  ],
+  transformIgnorePatterns: [combinedModules],
 };
