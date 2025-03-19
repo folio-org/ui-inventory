d1a87d026292cd937c95a440f34d9209d561e357
mariia-aloshyna <mariia_aloshyna@epam.com>
2025-03-19 15:59:51 +0200

    UIIN-3286: Refactor `useItemAuditDataQuery` hook to use `useInfiniteQuery`

--- src/hooks/useItemAuditDataQuery/useItemAuditDataQuery.js
+++ src/hooks/useItemAuditDataQuery/useItemAuditDataQuery.js
@@ -1,30 +1,62 @@
-import { useQuery } from 'react-query';
+import { useInfiniteQuery } from 'react-query';
+import { useMemo } from 'react';
 
 import {
   useNamespace,
   useOkapiKy,
 } from '@folio/stripes/core';
 
-const useItemAuditDataQuery = (itemId, eventTs) => {
+const useItemAuditDataQuery = (itemId) => {
   const ky = useOkapiKy();
   const [namespace] = useNamespace({ key: 'item-audit-data' });
 
-  // eventTs param is used to load more data
-  const { isLoading, data = {} } = useQuery({
-    queryKey: [namespace, itemId, eventTs],
-    queryFn: () => ky.get(`audit-data/inventory/item/${itemId}`, {
+  const {
+    data,
+    isLoading,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+    ...rest
+  } = useInfiniteQuery({
+    queryKey: [namespace, itemId],
+    queryFn: ({ pageParam }) => ky.get(`audit-data/inventory/item/${itemId}`, {
       searchParams: {
-        ...(eventTs && { eventTs })
+        ...(pageParam && { eventTs: pageParam }),
       }
     }).json(),
+    getNextPageParam: (lastPage, allPages) => {
+      const totalRecords = lastPage?.totalRecords || 0;
+      const totalFetchedItems = allPages.reduce((sum, page) => {
+        return sum + (page?.inventoryAuditItems?.length || 0);
+      }, 0);
+
+      // if we've fetched all records, return undefined to disable further loading
+      if (totalFetchedItems >= totalRecords) {
+        return undefined;
+      }
+
+      // otherwise, return the timestamp of the last item for the next page
+      const items = lastPage?.inventoryAuditItems || [];
+      return items.length > 0 ? items[items.length - 1].eventTs : undefined;
+    },
     enabled: Boolean(itemId),
     cacheTime: 0,
   });
 
+  // flatten all pages into a single array
+  const flattenedData = useMemo(() => {
+    return data?.pages?.flatMap(page => page?.inventoryAuditItems || []) || [];
+  }, [data]);
+  const totalRecords = data?.pages?.[0]?.totalRecords || 0;
+
   return {
-    data: data?.inventoryAuditItems || [],
-    totalRecords: data?.totalRecords,
+    data: flattenedData,
+    totalRecords,
     isLoading,
+    isLoadingMore: isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+    ...rest,
   };
 };
 
