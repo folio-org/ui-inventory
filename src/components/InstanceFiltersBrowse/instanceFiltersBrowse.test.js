cfd1f2db433690973e6897fabac0b88dc2f90b5a
Denys Bohdan <denys_bohdan@epam.com>
2025-04-03 12:44:07 +0200

    UIIN-3301 Call number browse | Remove held by facet for ECS (#2808)

--- src/components/InstanceFiltersBrowse/instanceFiltersBrowse.test.js
+++ src/components/InstanceFiltersBrowse/instanceFiltersBrowse.test.js
@@ -138,7 +138,7 @@ describe('InstanceFiltersBrowse', () => {
   });
 
   describe('When callNumber browseType was selected', () => {
-    it('should display "Held By" facet accordion', () => {
+    it.skip('should display "Held By" facet accordion', () => {
       const { getByRole } = renderInstanceFilters({
         data,
         query: {
