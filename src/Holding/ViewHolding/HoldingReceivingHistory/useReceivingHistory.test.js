9e2251f521ef094548610c95c2f33a804aebd2f7
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2025-04-23 21:39:43 +0200

    UIIN-3211: Remove extra record from Central tenant after adding Receiving history record in local member instance

--- src/Holding/ViewHolding/HoldingReceivingHistory/useReceivingHistory.test.js
+++ src/Holding/ViewHolding/HoldingReceivingHistory/useReceivingHistory.test.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { act } from 'react';
 import {
   QueryClient,
   QueryClientProvider,
 } from 'react-query';
-import { renderHook, act } from '@folio/jest-config-stripes/testing-library/react';
+import { renderHook } from '@folio/jest-config-stripes/testing-library/react';
 
 import '../../../../test/jest/__mock__';
 
-import { useOkapiKy } from '@folio/stripes/core';
+import { useOkapiKy, useStripes } from '@folio/stripes/core';
 
 import { piece, receivingHistory } from './fixtures';
 import useReceivingHistory from './useReceivingHistory';
+
+const holdingWithReceivingHistory = {
+  id: 'holdingId',
+  receivingHistory: { entries: [receivingHistory[0]] },
+};
 
 const queryClient = new QueryClient();
 const wrapper = ({ children }) => (
@@ -21,19 +26,58 @@ const wrapper = ({ children }) => (
 
 describe('useReceivingHistory', () => {
   beforeEach(() => {
+    jest.clearAllMocks();
+
     useOkapiKy.mockClear().mockReturnValue({
       get: () => ({
         json: () => Promise.resolve({ pieces: [piece] }),
       }),
     });
+
+    useStripes.mockReturnValue({
+      user: { user: { consortium: { centralTenantId: 'centralTenantId' } } },
+      hasInterface: () => true,
+    });
   });
 
   it('should return receiving history based on pieces and holding history', async () => {
-    const holding = { id: 'holdingId', receivingHistory: { entries: [receivingHistory[0]] } };
-    const { result } = renderHook(() => useReceivingHistory(holding), { wrapper });
+    const { result } = renderHook(() => useReceivingHistory(holdingWithReceivingHistory), { wrapper });
 
     await act(() => !result.current.isLoading);
 
     expect(result.current.receivingHistory).toEqual(receivingHistory);
+  });
+
+  describe('when user is central tenant', () => {
+    it('should return receiving history based on pieces from API', async () => {
+      const holdingWithoutReceivingHistory = {
+        id: 'holdingId',
+        receivingHistory: {},
+      };
+
+      const { result } = renderHook(
+        () => useReceivingHistory(holdingWithoutReceivingHistory, 'centralTenantId'),
+        { wrapper },
+      );
+
+      await act(() => !result.current.isFetching);
+
+      expect(result.current.receivingHistory).toEqual([
+        { ...piece, source: 'receiving' },
+      ]);
+    });
+  });
+
+  describe('when user is member tenant', () => {
+    it('should return receiving history based on pieces and holding history', async () => {
+      const { result } = renderHook(
+        () => useReceivingHistory(holdingWithReceivingHistory, 'memberTenantId'),
+        { wrapper },
+      );
+
+      await act(() => !result.current.isFetching);
+
+      expect(result.current.receivingHistory).toEqual(receivingHistory);
+    });
   });
 });
