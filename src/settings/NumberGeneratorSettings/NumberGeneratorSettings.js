dd3089b4e981b5b5fb5f8cdeebbc32afe464c06e
elsenhans <elsenhans@users.noreply.github.com>
2025-09-10 12:00:46 +0200

    UIIN-3471-add-default-values

--- src/settings/NumberGeneratorSettings/NumberGeneratorSettings.js
+++ src/settings/NumberGeneratorSettings/NumberGeneratorSettings.js
@@ -5,17 +5,28 @@ import { useStripes } from '@folio/stripes/core';
 import { ConfigManager } from '@folio/stripes/smart-components';
 
 import {
+  ACCESSION_NUMBER_SETTING,
+  BARCODE_SETTING,
+  CALL_NUMBER_SETTING,
+  NUMBER_GENERATOR_OPTIONS_OFF,
   NUMBER_GENERATOR_SETTINGS_KEY,
   NUMBER_GENERATOR_SETTINGS_SCOPE,
 } from './constants';
 import NumberGeneratorSettingsForm from './NumberGeneratorSettingsForm';
+
+const DEFAULT_VALUES = {
+  [ACCESSION_NUMBER_SETTING]: NUMBER_GENERATOR_OPTIONS_OFF,
+  [BARCODE_SETTING]: NUMBER_GENERATOR_OPTIONS_OFF,
+  [CALL_NUMBER_SETTING]: NUMBER_GENERATOR_OPTIONS_OFF,
+  [`${CALL_NUMBER_SETTING}Holdings`]: NUMBER_GENERATOR_OPTIONS_OFF,
+};
 
 const NumberGeneratorSettings = () => {
   const stripes = useStripes();
   const ConnectedConfigManager = useMemo(() => stripes.connect(ConfigManager), [stripes]);
 
   const beforeSave = (data) => data || '';
-  const getInitialValues = (settings) => (settings?.[0]?.value || '');
+  const getInitialValues = (settings) => (settings?.[0]?.value || DEFAULT_VALUES);
 
   return (
     <ConnectedConfigManager
