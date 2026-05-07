2940ec6980d0c9fd1feff7a7a600826036e723d5
Oleksandr Hladchenko <85172747+OleksandrHladchenko1@users.noreply.github.com>
2026-05-07 13:48:12 +0200

    UIIN-3604: Show error message when edit URLs are with invalid UUIDs (#3017)
    * UIIN-3604: Show error message when edit URLs are with invalid UUIDs
    
    * Update EditHoldingRoute.js
    
    Co-authored-by: Mariia Aloshyna <55138456+mariia-aloshyna@users.noreply.github.com>
    
    * Update EditItemRoute.js
    
    Co-authored-by: Mariia Aloshyna <55138456+mariia-aloshyna@users.noreply.github.com>
    
    * Update InstanceEditRoute.js
    
    Co-authored-by: Mariia Aloshyna <55138456+mariia-aloshyna@users.noreply.github.com>
    
    * UIIN-3604: Fix tests
    
    ---------
    
    Co-authored-by: Mariia Aloshyna <55138456+mariia-aloshyna@users.noreply.github.com>
    (cherry picked from commit 2b896076a3deca21df301b5dcf1f2904b89e7c47)

--- src/routes/EditItemRoute.js
+++ src/routes/EditItemRoute.js
@@ -1,12 +1,23 @@
 import React, { useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
+
+import { AuthenticatedError } from '@folio/stripes/core';
+import { isValidUUID } from '@folio/stripes/util';
 
 import { EditItem } from '../Item';
 import { DataContext } from '../contexts';
 
 const EditItemRoute = () => {
+  const location = useLocation();
   const { id, holdingId, itemId } = useParams();
+
   const referenceData = useContext(DataContext);
+
+  const hasValidParams = [id, holdingId, itemId].every(isValidUUID);
+
+  if (!hasValidParams) {
+    return <AuthenticatedError location={location} />;
+  }
 
   return (
     <EditItem
